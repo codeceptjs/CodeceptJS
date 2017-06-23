0c771f505dd2b1b18ffe1de6e047d6b33311e3a9
Pshenkin Andrey <a.pshenkin@rambler-co.ru>
2017-06-23 14:33:28 +0300

    refactor work with chains

--- test/runner/interface_test.js
+++ test/runner/interface_test.js
@@ -30,32 +30,22 @@ describe('CodeceptJS Interface', () => {
       var queue1 = stdout.match(/\[1\] .+/g);
       queue1.should.eql([
         "[1] Starting recording promises",
-        "[1] Queued | hook FileSystem._beforeSuite()"
+        "[1] Queued | hook FileSystem._beforeSuite()",
+        `[1] Queued | hook FileSystem._before()`,
+        `[1] Queued | amInPath: "."`,
+        `[1] Queued | return step result`,
+        `[1] Queued | say hello world`,
+        `[1] Queued | seeFile: "codecept.json"`,
+        `[1] Queued | return step result`,
+        `[1] Queued | fire test.passed`,
+        `[1] Queued | finish test`,
+        `[1] Queued | hook FileSystem._after()`
       ]);
 
       var queue2 = stdout.match(/\[2\] .+/g);
       queue2.should.eql([
         `[2] Starting recording promises`,
-        `[2] Queued | hook FileSystem._before()`,
-        `[2] Queued | amInPath: "."`,
-        `[2] Queued | return step result`,
-        `[2] Queued | say hello world`,
-        `[2] Queued | seeFile: "codecept.json"`,
-        `[2] Queued | return step result`,
-        `[2] Queued | fire test.passed`,
-        `[2] Queued | finish test`
-      ]);
-
-      var queue3 = stdout.match(/\[3\] .+/g);
-      queue3.should.eql([
-        `[3] Starting recording promises`,
-        `[3] Queued | hook FileSystem._after()`
-      ]);
-
-      var queue4 = stdout.match(/\[4\] .+/g);
-      queue4.should.eql([
-        `[4] Starting recording promises`,
-        `[4] Queued | hook FileSystem._afterSuite()`
+        `[2] Queued | hook FileSystem._afterSuite()`,
       ]);
 
       let lines = stdout.match(/\S.+/g);
@@ -63,7 +53,6 @@ describe('CodeceptJS Interface', () => {
       // before hooks
       let beforeStep = [
         `Emitted | step.before (I am in path ".")`,
-        `[2] Queued | amInPath: "."`,
         `Emitted | step.after (I am in path ".")`,
         `Emitted | step.start (I am in path ".")`,
         `• I am in path "."`
