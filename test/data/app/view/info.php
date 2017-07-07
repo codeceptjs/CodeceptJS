f083b8fdb48f10975b11ee4cb283290dc24eaecb
Pshenkin Andrey <a.pshenkin@rambler-co.ru>
2017-07-07 19:22:15 +0300

    revert changes in info.php

--- test/data/app/view/info.php
+++ test/data/app/view/info.php
@@ -35,21 +35,10 @@
     Invisible text
 </div>
 
-<input type="email" name="email" id="email" value="wow" tabindex="1" autofocus="autofocus" class="text">
-<a id="first-link" hidden="true">First</a>
 <div id="grab-multiple">
     <a id="first-link">First</a>
     <a id="second-link">Second</a>
     <a id="third-link">Third</a>
-</div>
-
-
-<div id="within-test">
-    <a id="first-link">First</a>
-    <a id="second-link">False Second</a>
-    <a id="third-link">Third</a>
-    <a href="/login">Ссылочка</a>
-    <input type="email" name="email" id="email" value="much test" tabindex="1" autofocus="autofocus" class="text">
 </div>
 
 </body>
