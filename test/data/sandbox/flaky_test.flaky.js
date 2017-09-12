1a0468a7b39ec441c5ec45b791e9543769f6ada1
Drew Diamantoukos <drewd@code-phoenix.com>
2017-09-12 05:33:25 -0400

    Typings (#699)
    * Fixing
    
    * Updaing helper code
    
    * Home stretch for compiler fixes, just two left
    
    * Touched up tests and Mocha Base reporter class type
    
    * Adding some lint-related fixes for nightmarejs and appium
    
    * Using value in Nightmare test instead of nodeValue
    
    * Reverted Appium default config initialization location. Fixed waitFor methods in Nightmare to handle default parameter.
    
    * Added the package-lock generated by npm 5
    
    * Reverted using sec for parameter name for wait Helper methods

--- test/data/sandbox/flaky_test.flaky.js
+++ test/data/sandbox/flaky_test.flaky.js
@@ -2,15 +2,15 @@ var assert = require('assert');
 var tries = 0;
 var tries2 = 0;
 
-Feature('Flacky', {retries: 4});
+Feature('Flaky', {retries: 4});
 
-Scenario('Not so flacky test', { retries: 2}, () => {
+Scenario('Not so flaky test', { retries: 2}, () => {
   tries++;
   assert.equal(tries, 2);
   console.log('[T1] Retries: '+tries);
 });
 
-Scenario('Really flacky test', () => {
+Scenario('Really flaky test', () => {
   tries2++;
   assert.equal(tries2, 4);
   console.log('[T2] Retries: '+tries2);
---
+++ typings/index.d.ts
@@ -0,0 +1,74 @@
+// Type definitions for CodeceptJS 1.0.2
+// Project: https://github.com/codeception/codeceptjs/
+// Definitions by: Michael Bodnarchuk <http://github.com/DavertMik>, Drew Diamantoukos <https://github.com/KennyRules>
+
+declare module NodeJS {
+  interface Process {
+    profile: string;
+  }
+
+  interface Global {
+    codecept_actor: any;
+    codecept_dir: string;
+    codecept_helper: any;
+    output_dir: string;
+
+    actor: any;
+    Helper: any;
+    pause: any;
+    within: any;
+    DataTable: any;
+    by: any;
+
+    // Used by Protractor helper
+    By: any;
+    ExpectedConditions: any;
+    element: any;
+    $: any;
+    $$: any;
+    browser: any;
+  }
+}
+
+declare interface Window {
+  codeceptjs: any;
+}
+
+declare var window: Window;
+
+/**
+ * Special Mocha definitions for reporter Base and Suite.
+ * The mocha type definitions on DefinitelyTyped are for an older version of Mocha!
+ */
+
+declare module 'mocha/lib/reporters/base' {
+  class base {
+    constructor(runner: any);
+    static cursor: any;
+    static color: any;
+    static list(items: any);
+    static symbols: any;
+    failures: any;
+    stats: any;
+  }
+
+  export = base;
+}
+
+declare interface Suite {
+  addTest: any;
+  afterAll: any;
+  afterEach: any;
+  beforeAll: any;
+  beforeEach: any;
+  pending: any;
+  on: any;
+  timeout: any;
+  title: any;
+
+  create(suite: any, title: any): any;
+}
+
+declare module 'mocha/lib/suite' {
+  export = Suite;
+}
