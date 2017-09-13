720e7c0e3bb7c6315408198ccaa15521fc12d6fc
Boris Osipov <osipov.boris@gmail.com>
2017-09-13 13:00:54 +0300

    Fix "[object Object] was not found by.." error message - changes after review - replace static method with separate class - add unit tests

---
+++ test/unit/helper/element_not_found_test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const ElementNotFound = require('../../../lib/helper/errors/ElementNotFound');
+const expect = require('chai').expect;
+const locator = "#invalidSelector";
+
+
+describe('ElementNotFound error', () => {
+
+  it('should throw error', () => {
+    expect(()=> new ElementNotFound(locator)).to.throw(Error);
+  });
+
+  it('should provide default message', () => {
+    expect(()=> new ElementNotFound(locator))
+      .to.throw(Error, "Element #invalidSelector was not found by text|CSS|XPath");
+  });
+
+  it('should use prefix for message', () => {
+    expect(()=> new ElementNotFound(locator, "Field"))
+      .to.throw(Error, "Field #invalidSelector was not found by text|CSS|XPath");
+  });
+
+  it('should use postfix for message', () => {
+    expect(()=> new ElementNotFound(locator, "Locator", "cannot be found"))
+      .to.throw(Error, "Locator #invalidSelector cannot be found");
+  });
+
+  it('should stringify locator object', () => {
+    const objectLocator = {css: locator};
+    expect(()=> new ElementNotFound(objectLocator))
+      .to.throw(Error, `Element ${JSON.stringify(objectLocator)} was not found by text|CSS|XPath`);
+  });
+
+});
