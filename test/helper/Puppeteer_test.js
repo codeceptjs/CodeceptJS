58a6a4b34c086e6c52223de28b725738ca76e591
Davert <davert.php@resend.cc>
2017-12-06 03:21:56 +0200

    first Puppeteer tests

--- test/helper/Puppeteer_test.js
+++ test/helper/Puppeteer_test.js
@@ -1,37 +1,35 @@
-'use strict';
-let TestHelper = require('../support/TestHelper');
-let Puppeteer = require('../../lib/helper/Puppeteer');
-let should = require('chai').should();
-let I, browser, page;
-let site_url = TestHelper.siteUrl();
-let assert = require('assert');
-let path = require('path');
-let fs = require('fs');
-let fileExists = require('../../lib/utils').fileExists;
-let AssertionFailedError = require('../../lib/assert/error');
-let formContents = require('../../lib/utils').test.submittedData(path.join(__dirname, '/../data/app/db'));
-let expectError = require('../../lib/utils').test.expectError;
-let webApiTests = require('./webapi');
 
-describe('Puppeteer', function () {
+const TestHelper = require('../support/TestHelper');
+const Puppeteer = require('../../lib/helper/Puppeteer');
+const should = require('chai').should();
+const assert = require('assert');
+const path = require('path');
+const fs = require('fs');
+const fileExists = require('../../lib/utils').fileExists;
+const AssertionFailedError = require('../../lib/assert/error');
+const formContents = require('../../lib/utils').test.submittedData(path.join(__dirname, '/../data/app/db'));
+const expectError = require('../../lib/utils').test.expectError;
+const webApiTests = require('./webapi');
 
-  before(function() {
+let I;
+let browser;
+let page;
+const siteUrl = TestHelper.siteUrl();
+
+describe('Puppeteer', () => {
+  before(() => {
     global.codecept_dir = path.join(__dirname, '/../data');
-    try {
-      fs.unlinkSync(dataFile);
-    } catch (err) {}
-
     I = new Puppeteer({
-      url: site_url,
+      url: siteUrl,
       windowSize: '500x400',
-      show: false
+      show: false,
     });
     I._init();
-    I._beforeSuite();
+    return I._beforeSuite();
   });
 
-  beforeEach(function() {
-    // webApiTests.init({ I, site_url});
+  beforeEach(() => {
+    // webApiTests.init({ I, siteUrl});
     return I._before().then(() => {
       page = I.page;
       browser = I.browser;
@@ -45,22 +43,44 @@ describe('Puppeteer', function () {
   describe('open page : #amOnPage', () => {
     it('should open main page of configured site', async () => {
       await I.amOnPage('/');
-      let url = await page.url();
-      await url.should.eql(site_url + '/');
+      const url = await page.url();
+      await url.should.eql(`${siteUrl}/`);
     });
     it('should open any page of configured site', async () => {
       await I.amOnPage('/info');
-      let url = await page.url();
-      return url.should.eql(site_url + '/info');
+      const url = await page.url();
+      return url.should.eql(`${siteUrl}/info`);
     });
 
     it('should open absolute url', async () => {
-      await I.amOnPage(site_url);
-      let url = await page.url();
-      return url.should.eql(site_url + '/');
+      await I.amOnPage(siteUrl);
+      const url = await page.url();
+      return url.should.eql(`${siteUrl}/`);
     });
   });
+  describe('see element : #seeElement, #seeElementInDOM, #dontSeeElement', () => {
+    it('should check visible elements on page', function* () {
+      yield I.amOnPage('/form/field');
+      yield I.seeElement('input[name=name]');
+      yield I.seeElement({ name: 'name' });
+      yield I.seeElement('//input[@id="name"]');
+      yield I.dontSeeElement('#something-beyond');
+      return I.dontSeeElement('//input[@id="something-beyond"]');
+    });
 
+    it('should check elements are in the DOM', function* () {
+      yield I.amOnPage('/form/field');
+      yield I.seeElementInDOM('input[name=name]');
+      yield I.seeElementInDOM('//input[@id="name"]');
+      yield I.dontSeeElementInDOM('#something-beyond');
+      return I.dontSeeElementInDOM('//input[@id="something-beyond"]');
+    });
 
-
+    it('should check elements are visible on the page', function* () {
+      yield I.amOnPage('/form/field');
+      yield I.seeElementInDOM('input[name=email]');
+      yield I.dontSeeElement('input[name=email]');
+      return I.dontSeeElement('#something-beyond');
+    });
+  });
 });
