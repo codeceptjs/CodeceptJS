315d02326bf514a5ba868854fdfc5444ee8b17fd
Davert <davert.php@resend.cc>
2017-12-08 04:50:56 +0200

    added Puppeteer tests

--- test/helper/Puppeteer_test.js
+++ test/helper/Puppeteer_test.js
@@ -18,6 +18,7 @@ const siteUrl = TestHelper.siteUrl();
 
 describe('Puppeteer', function () {
   this.timeout(35000);
+  this.retries(1);
 
   before(() => {
     global.codecept_dir = path.join(__dirname, '/../data');
