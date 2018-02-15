5091a326dde585ef7f61efac902cdeb136aebc68
Reuben Miller <reuben.d.miller@gmail.com>
2018-02-15 19:07:08 +0100

    [Tests] Improving reliability of the CI tests (Travis and Circle CI) (#940)
    * Adding chrome headless option when running tests and updating the selenium standalone chrome image
    
    * [lint] Added missing semicolon
    
    * [Puppeteer] Adding delays after switching tabs in tests
    
    * Adding DBUS_SESSION_BUS_ADDRESS=/dev/null to travis environment for the Nightmare because it was timing out sometimes
    
    * Refactoring apiDataFactory_test to async/await instead of generators
    
    * Removed console message
    
    * Added retry to the within acceptence tests
    
    * Increased retries to 3 for the within acceptance tests due to it failing in circle ci
    
    * Adding a few small delays to Puppeteer Tests when closing tabs
    
    * Adding DBUS env variable to circle config due to timeout error
    
    * Trigger ci 1
    
    * Trigger ci 2
    
    * Trigger ci 3
    
    * Trigger ci 4
    
    * Trigger ci 5
    
    * Trigger ci 6
    
    * Removed the rest tests from the travis build
    
    * Trigger ci 1
    
    * Trigger ci 2
    
    * Removing waitUnitExists tests because it is a duplicate of waitForElement

--- test/helper/Protractor_test.js
+++ test/helper/Protractor_test.js
@@ -34,6 +34,11 @@ describe('Protractor', function () {
       seleniumAddress: TestHelper.seleniumAddress(),
       angular: true,
       waitForTimeout: 5000,
+      desiredCapabilities: {
+        chromeOptions: {
+          args: ['--headless', '--disable-gpu', '--window-size=1280,1024'],
+        },
+      },
     });
     return I._init().then(() => I._beforeSuite());
   });
