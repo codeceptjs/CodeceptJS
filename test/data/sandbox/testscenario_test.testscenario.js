2dcd04bf0ab090037fbd42408da20f0f00b3b7f7
Pshenkin Andrey <a.pshenkin@rambler-co.ru>
2018-02-27 16:25:48 +0300

    add test for async function without brackets

--- test/data/sandbox/testscenario_test.testscenario.js
+++ test/data/sandbox/testscenario_test.testscenario.js
@@ -13,3 +13,8 @@ Scenario('Simple async/await test', async (I) => {
   const text = await I.stringWithScenarioType('async/await');
   console.log(text);
 });
+
+Scenario('Should understand async without brackets', async (I) => {
+  const text = await I.stringWithScenarioType('asyncbrackets');
+  console.log(text);
+});
