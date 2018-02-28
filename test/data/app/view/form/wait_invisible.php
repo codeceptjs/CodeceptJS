13e1ffa96b97c5fe53918175898b932be84bebbf
Reuben Miller <reuben.d.miller@gmail.com>
2018-02-28 19:09:12 +0100

    [Helpers] Changed I.waitForInvisible so that it does not differentiate between an element being visible or in the DOM, and all of the helpers behave the same now (with tests)

--- test/data/app/view/form/wait_invisible.php
+++ test/data/app/view/form/wait_invisible.php
@@ -8,11 +8,13 @@
 <body>
 
 <div id="step_1">Step One Button</div>
+<div id="step_2">Step Two Button</div>
 <script>
   setTimeout(function () {
     document.getElementById('step_1').style.display = 'none';
+    var step2 = document.getElementById('step_2');
+    step2.parentElement.removeChild(step2);
   }, 1000);
-
 
 </script>
 
