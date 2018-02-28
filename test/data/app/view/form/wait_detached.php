f2960d98b9c68c144bcae208771ae156473552a8
Reuben Miller <reuben.d.miller@gmail.com>
2018-02-28 19:10:13 +0100

    [Helpers] Added test cases for I.waitForDetached and changed error messages so they are they same across all helpers

---
+++ test/data/app/view/form/wait_detached.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <style>
+        .invisible_button { display: none; }
+    </style>
+</head>
+
+<body>
+
+<div id="step_1">Step One Button</div>
+<div id="step_2">Step Two Button</div>
+<script>
+  setTimeout(function () {
+    document.getElementById('step_1').style.display = 'none'; // Hide button
+    var step2 = document.getElementById('step_2'); // Remove button
+    step2.parentElement.removeChild(step2);
+  }, 1000);
+
+</script>
+
+</body>
+</html>
