6edf0fabf4ba95853ae6f0cd6c1468b704504931
Davert <davert.php@resend.cc>
2018-03-05 20:27:30 +0200

    build and run separated

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -4,7 +4,6 @@ set -e
 
 cd test
 
-docker-compose build
 docker-compose run --rm test-helpers test/helper
 docker-compose run --rm test-helpers test/rest
 docker-compose run --rm test-acceptance.webdriverio
