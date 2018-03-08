627ddf9dd4b6aca280ce69b9d0ab86121a57c9a0
Sven Eisenschmidt <sven.eisenschmidt@trivago.com>
2018-03-08 20:06:03 +0100

    [feature] Introduce chunking to run-multiple - added: suite.prepareSuites - creates unified suites coniguration based on preferredSuites - added: suite.prepareSuitesChunks - expands configuration by (n) chunks if chunks are configured - added: suite.prepareSuitesBrowsers - expands configuration by (n) browsers if browsers are configured - added: suite.filterSuitesBrowsers - filters configuration by requested browsers set via preferredSuites - updated: runSuite   - browser argument is removed, it is now retrieved through a unified browser config   - forking over multiple browsers is removed, it became obsolete as soon as we prepared     and expanded the whole configuration beforehand, including chunks and browsers

--- test/runner/run_multiple_test.js
+++ test/runner/run_multiple_test.js
@@ -15,6 +15,7 @@ describe('CodeceptJS Multiple Runner', function () {
 
   it('should execute one suite with browser', (done) => {
     exec(`${codecept_run}default:firefox`, (err, stdout, stderr) => {
+      console.log(stderr);
       stdout.should.include('CodeceptJS'); // feature
       stdout.should.include('.default:firefox] print browser ');
       stdout.should.not.include('.default:chrome] print browser ');
