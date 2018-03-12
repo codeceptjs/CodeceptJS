265619ef0976e1ef2bef52f7485be5bfc981f90e
Reuben Miller <reuben.d.miller@gmail.com>
2018-03-12 23:50:16 +0100

    [Helper] Protractor feature parity and bug fixes (#995)
    * Added seeNumberOfVisibleElements and grabNumberOfVisibleElements
    
    * [Nightmare] Added grabNumberOfVisibleElements and seeNumberOfVisibleElements and moved tests to webapi for all Helpers
    
    * Adding waitInUrl and waitUrlEquals to Protractor and moving tests to webapi
    
    * Adding waitToHide to Protractor and Nightmare
    
    * Added waitToHide to protractor and webapi tests
    
    * Adding I.switchTo to Protractor and bug fixes when using a frame index number of 0 in Puppeteer and WebDriverIO
    
    * Removing unused code and arguments
    
    * Lint fixes
    
    * [Protractor] Adding support for nested iframes
    
    * Adding acceptance testing for Protractor in Circle CI Tests
    
    * Spliting nightmare webapi tests out whilst concentrating on Protractor func parity
    
    * [Protractor] Fixing bug where _failed would throw an error if I.amOnPage was never called
    
    * [Protractor] Added waitNumberOfVisibleElements and waitForEnabled
    
    * Fixing mistake when waitNumberOfVisibleElements
    
    * [Protractor] Adding waitForValue
    
    * [Protractor] Adding waitUtil and grabHTMLFrom
    
    * [Protractor] Adding popup functions
    
    * [Puppeteer] Fixing executeScript context bug when using iframes
    
    * Scrolling: Adding new grabPageScrollPosition and scroll* functions to Protractor as well as fixing behaviour across all of the web helpers (including tests)
    
    * [Protractor] Adding I.rightClick
    
    * [Protractor] Adding I.dragAndDrop
    
    * [Protractor] Added I.grabBrowserLogs and fixing incorrect property name for capabilities
    
    * [Protractor] Added I.grabCssPropertyFrom and moved tests for other web helpers (except for Nightmare) to the central webapi test file
    
    * [Protractor] Added I.seeAttributesOnElements and moved tests for puppeteer and webdriverio to the common webapi tests file
    
    * [Protractor] Added I.cssPropertiesOnElements and moved tests for puppeteer and webdriverio to the common webapi tests file
    
    * Lint fix
    
    * [Protractor] Adding _locate* functions
    
    * Consolodated the webapi tests and added skip logic for Nightmare
    
    * Added css property color normalisation as the web drivers all return different formats.
    
    * Moved color normalisation to seperate util file and added more test cases for it
    
    * Fixed bug when comparing the font weight property noramlization

---
+++ test/data/app/view/form/css_colors.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>CSS Color Properties</title>
+  <style>
+  #namedColor {
+    background: purple; /* purple, #800080, rgb(128,0,128), rgba(128,0,128,1) */
+    color: yellow; /* yellow, #ffff00, rgb(255,255,0), rgba(255,255,0,1) */
+  }
+  #hexColor {
+    background: #00ffff; /* cyan, #00ffff, rgb(0,255,255), rgba(0,255,255,1) */
+    color: #a8a810;
+  }
+  
+  #rgbColor {
+    background: rgb(255,105,180); /* hotpink, #ff69b4, rgb(255,105,180), rgba(255,105,180,1) */
+    color: #a8a810;
+  }
+
+  #rgbaColor {
+    background: rgba(255,99,71,0.8); /* rgba(255,105,180,1) */
+    color: black; /* black, #000000, rgb(0,0,0), rgba(0,0,0,1) */
+  }
+  </style>
+</head>
+<body>
+
+<div id="block"></div>
+<h1 id="title">CSS Color Properties</h1>
+<h2 id="namedColor">Blue Header</h2>
+<h2 id="hexColor">Header using hex code</h2>
+<h2 id="rgbColor">Header using rgb</h2>
+<h2 id="rgbaColor">Header using rgba</h2>
+
+
+<script>
+</script>
+
+</body>
+</html>
