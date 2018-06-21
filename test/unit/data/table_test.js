fe1ded6ae55f0a27ca14e647cf784e6628e54e02
Evgeniy Lukoyanov <LukoyanovE@users.noreply.github.com>
2018-06-21 15:07:33 +0300

    Feature: add filter function to DataTable (#1101)
    * Updated typescipt definitions.
    
    * Fixed test runner for nested test directories
    
    * Fixed test runner for nested test directories

--- test/unit/data/table_test.js
+++ test/unit/data/table_test.js
@@ -39,4 +39,50 @@ describe('DataTable', () => {
     const dataTable = new DataTable(data);
     assert.throws(() => dataTable.add(['Acid', 'Jazz', 'Singer']));
   });
+
+  it('should filter an array', () => {
+    const data = ['login', 'password'];
+    const dataTable = new DataTable(data);
+    dataTable.add(['jon', 'snow']);
+    dataTable.add(['tyrion', 'lannister']);
+    dataTable.add(['jaime', 'lannister']);
+
+    const expected = [{
+      skip: false,
+      data: {
+        login: 'tyrion',
+        password: 'lannister',
+      },
+    }, {
+      skip: false,
+      data: {
+        login: 'jaime',
+        password: 'lannister',
+      },
+    }];
+    assert.equal(JSON.stringify(dataTable.filter(row => row.password === 'lannister')), JSON.stringify(expected));
+  });
+
+  it('should filter an array with skips', () => {
+    const data = ['login', 'password'];
+    const dataTable = new DataTable(data);
+    dataTable.add(['jon', 'snow']);
+    dataTable.xadd(['tyrion', 'lannister']);
+    dataTable.add(['jaime', 'lannister']);
+
+    const expected = [{
+      skip: true,
+      data: {
+        login: 'tyrion',
+        password: 'lannister',
+      },
+    }, {
+      skip: false,
+      data: {
+        login: 'jaime',
+        password: 'lannister',
+      },
+    }];
+    assert.equal(JSON.stringify(dataTable.filter(row => row.password === 'lannister')), JSON.stringify(expected));
+  });
 });
