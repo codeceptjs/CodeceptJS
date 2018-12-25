bc57afe586b44f67f13571ee3d5081df85d3f21b
Michael Bodnarchuk <DavertMik@users.noreply.github.com>
2018-12-25 14:25:36 +0200

    Remove generators (#1387)
    * removed generators support from scenario and within
    
    * removed test for generators
    
    * updated test for within to remove generators
    
    * removed generator tests

--- test/data/sandbox/testhooks_test.testhooks.js
+++ test/data/sandbox/testhooks_test.testhooks.js
@@ -16,23 +16,23 @@ AfterSuite(() => {
   console.log('Test: I\'m simple AfterSuite hook');
 });
 
-BeforeSuite(function* (I) {
-  const text = yield I.stringWithHook('BeforeSuite');
+BeforeSuite(async (I) => {
+  const text = await I.stringWithHook('BeforeSuite');
   console.log(text);
 });
 
-Before(function* (I) {
-  const text = yield I.stringWithHook('Before');
+Before(async (I) => {
+  const text = await I.stringWithHook('Before');
   console.log(text);
 });
 
-After(function* (I) {
-  const text = yield I.stringWithHook('After');
+After(async (I) => {
+  const text = await I.stringWithHook('After');
   console.log(text);
 });
 
-AfterSuite(function* (I) {
-  const text = yield I.stringWithHook('AfterSuite');
+AfterSuite(async (I) => {
+  const text = await I.stringWithHook('AfterSuite');
   console.log(text);
 });
 
