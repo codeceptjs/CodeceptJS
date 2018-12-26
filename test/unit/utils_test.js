4228b5bfc81e91ce7bb1b81e50b150e6fdb4efac
Thanh Nguyen <thanh.nguyen@mcmakler.de>
2018-12-26 10:30:34 +0100

    cut data string when saving screenshot

--- test/unit/utils_test.js
+++ test/unit/utils_test.js
@@ -40,10 +40,4 @@ describe('utils', () => {
         .should.eql('concat(\'can\',"\'",\'t find thing\')');
     });
   });
-
-  describe('#clearString', () => {
-    it('should exclude the data driven in failed screenshot file name', () => {
-      utils.clearString('Scenario with data driven | {"login":"admin","password":"123456"}').should.equal('Scenario_with_data_driven');
-    });
-  });
 });
