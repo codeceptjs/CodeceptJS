28f0512a1f9d600a30c131b43d9a962349f94a6a
Peter Nguyen Tr <peter.nguyentr@gmail.com>
2019-01-28 13:43:48 +0100

    synced with master (#1)
    * Added WebDriver to autoDelay supported helpers (#1455)
    
    WebDriverIO is deprecated and should be removed from the list when the deprecation notices are removed.
    
    * Fix multiple-execution link (#1456)
    
    * add label to allure report + fix collect tag (#1449)
    
    * Skip test in allure-report (#1448)
    
    * Fix issue 1445 and 1443 (#1446)
    
    * Fix running tests with different data and with only
    
    * fix
    
    * remove only
    
    * fix comment
    
    * fix comments
    
    * fix issue 1443
    
    * use JSON.stringify for all obj
    
    * Change setValue() to addValue() (and add some tests) (#1460)
    
    * improved TS definitions
    
    * fixed the case when there are more than one matched element (#1453)
    
    * fixed the case when there are more than one matched element
    
    * fixed the same thing for nightmare and protractor
    
    * prepare release and website update

---
+++ test/data/sandbox/ddt_test.gddt.js
@@ -0,0 +1,8 @@
+Feature('ADDT');
+
+Data(function* () {
+  yield { user: 'nick' };
+  yield { user: 'pick' };
+}).only.Scenario('Should log generator of strings', (I, current) => {
+  console.log(`Got generator login ${current.user}`);
+});
