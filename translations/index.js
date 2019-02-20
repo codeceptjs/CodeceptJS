d8a6445ab3511966e78976f9a40cabc030294970
Takuya Suemura <takuyasuemura@gmail.com>
2019-02-20 23:26:32 +0900

    add japanese translation

--- translations/index.js
+++ translations/index.js
@@ -4,3 +4,4 @@ exports['it-IT'] = require('./it-IT');
 exports['pl-PL'] = require('./pl-PL');
 exports['zh-CN'] = require('./zh-CN');
 exports['zh-TW'] = require('./zh-TW');
+exports['ja-JP'] = require('./ja-JP');
