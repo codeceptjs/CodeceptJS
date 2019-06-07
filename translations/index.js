51bd501b3c2477309cf4fce09ef7db6e442c3aaa
Stefan Huber <hubidu@users.noreply.github.com>
2019-06-07 16:06:03 +0200

    Remove translation files

--- translations/index.js
+++ translations/index.js
@@ -5,4 +5,3 @@ exports['pl-PL'] = require('./pl-PL');
 exports['zh-CN'] = require('./zh-CN');
 exports['zh-TW'] = require('./zh-TW');
 exports['ja-JP'] = require('./ja-JP');
-exports['de-DE'] = require('./de-DE');
