ce73c2c7df8749eaecdfc12e8106cfc0ca818a26
VikalpP <vikalppanseriya@gmail.com>
2019-06-28 00:49:27 +0530

    test: add more test cases for different scenarios

--- test/acceptance/mock_test.js
+++ test/acceptance/mock_test.js
@@ -1,13 +1,44 @@
 Feature('Mocking');
 
-Scenario('mock requests @Puppeteer', (I) => {
+Scenario('change statusCode @Puppeteer', (I) => {
   I.amOnPage('/form/fetch_call');
-  I.click('GET DATA');
+  I.click('GET POSTS');
   I.startMocking();
   I.mock('GET', 'https://jsonplaceholder.typicode.com/*', 404);
-  I.click('GET DATA');
+  I.click('GET POSTS');
+  I.see('Can not load data!');
+});
+
+Scenario('change statusCode @Puppeteer', (I) => {
+  I.amOnPage('/form/fetch_call');
+  I.click('GET COMMENTS');
+  I.startMocking();
   I.mock('GET', 'https://jsonplaceholder.typicode.com/*', {
-    custom: 'MY DATA',
+    modified: 'This is modified from mocking',
   });
-  I.click('GET DATA');
+  I.click('GET COMMENTS');
+  I.see('This is modified from mocking', '#data');
+});
+
+Scenario('change statusCode for multiple requests @Puppeteer', (I) => {
+  I.amOnPage('/form/fetch_call');
+  I.click('GET POSTS');
+  I.startMocking();
+  I.mock(
+    'GET',
+    [
+      'https://jsonplaceholder.typicode.com/posts/*',
+      'https://jsonplaceholder.typicode.com/comments/*',
+      'https://jsonplaceholder.typicode.com/users/*',
+    ],
+    {
+      modified: 'MY CUSTOM DATA',
+    },
+  );
+  I.click('GET POSTS');
+  I.see('MY CUSTOM DATA', '#data');
+  I.click('GET COMMENTS');
+  I.see('MY CUSTOM DATA', '#data');
+  I.click('GET USERS');
+  I.see('MY CUSTOM DATA', '#data');
 });
--- test/data/app/view/form/fetch_call.php
+++ test/data/app/view/form/fetch_call.php
@@ -14,14 +14,15 @@
   </head>
   <body>
     <h3>JSON data</h3>
-    <button onclick="getJsonData()">GET DATA</button>
+    <button onclick="getPostData()">GET POSTS</button>
+    <button onclick="getCommentsData()">GET COMMENTS</button>
+    <button onclick="getUsersData()">GET USERS</button>
     <div id="data">
       <h4>No data here</h4>
     </div>
   </body>
 
   <script type="text/javascript">
-
     const tableData = data =>
       Object.entries(data).reduce(
         (html, [key, value]) => `${html} 
@@ -29,12 +30,14 @@
               <td>${key}</td>
               <td>${value}</td>
             </tr>
-          `, '');
+          `,
+        ""
+      );
 
     const data = document.querySelector("#data");
 
-    const getJsonData = () => 
-      fetch("https://jsonplaceholder.typicode.com/todos/1")
+    const getData = url =>
+      fetch(url)
         .then(response => response.json())
         .then(json => {
           data.innerHTML = `<table>
@@ -44,7 +47,14 @@
           console.log(json);
         })
         .catch(() => {
-          data.innerHTML = 'Can not load data!'
+          data.innerHTML = "Can not load data!";
         });
+
+    const getPostData = () =>
+      getData("https://jsonplaceholder.typicode.com/posts/1");
+    const getCommentsData = () =>
+      getData("https://jsonplaceholder.typicode.com/comments/1");
+    const getUsersData = () =>
+      getData("https://jsonplaceholder.typicode.com/users/1");
   </script>
 </html>
