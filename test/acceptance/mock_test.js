d687161fae5f795fb060d70cba27304981ebaa26
VikalpP <vikalppanseriya@gmail.com>
2019-07-02 01:13:40 +0530

    test: add test for stopMocking
    refactor: change testCases according to new method name I.mockRequest()

--- test/acceptance/mock_test.js
+++ test/acceptance/mock_test.js
@@ -1,30 +1,38 @@
 Feature('Mocking');
 
-Scenario('change statusCode @Puppeteer', (I) => {
-  I.amOnPage('/form/fetch_call');
-  I.click('GET POSTS');
-  I.startMocking();
-  I.mock('GET', 'https://jsonplaceholder.typicode.com/*', 404);
-  I.click('GET POSTS');
-  I.see('Can not load data!');
-});
+const fetchPost = response =>
+  response.url() === 'https://jsonplaceholder.typicode.com/posts/1';
+
+const fetchComments = response =>
+  response.url() === 'https://jsonplaceholder.typicode.com/comments/1';
 
 Scenario('change statusCode @Puppeteer', (I) => {
   I.amOnPage('/form/fetch_call');
+  I.click('GET POSTS');
+  I.waitForResponse(fetchPost, 3);
+  I.mockRequest('GET', 'https://jsonplaceholder.typicode.com/*', 404);
+  I.click('GET POSTS');
+  I.see('Can not load data!');
+  I.stopMocking();
+});
+
+Scenario('change response data @Puppeteer', (I) => {
+  I.amOnPage('/form/fetch_call');
   I.click('GET COMMENTS');
-  I.startMocking();
-  I.mock('GET', 'https://jsonplaceholder.typicode.com/*', {
+  I.waitForResponse(fetchComments, 3);
+  I.mockRequest('GET', 'https://jsonplaceholder.typicode.com/*', {
     modified: 'This is modified from mocking',
   });
   I.click('GET COMMENTS');
   I.see('This is modified from mocking', '#data');
+  I.stopMocking();
 });
 
-Scenario('change statusCode for multiple requests @Puppeteer', (I) => {
+Scenario('change response data for multiple requests @Puppeteer', (I) => {
   I.amOnPage('/form/fetch_call');
   I.click('GET POSTS');
-  I.startMocking();
-  I.mock(
+  I.waitForResponse(fetchPost, 3);
+  I.mockRequest(
     'GET',
     [
       'https://jsonplaceholder.typicode.com/posts/*',
@@ -41,4 +49,24 @@ Scenario('change statusCode for multiple requests @Puppeteer', (I) => {
   I.see('MY CUSTOM DATA', '#data');
   I.click('GET USERS');
   I.see('MY CUSTOM DATA', '#data');
+  I.stopMocking();
 });
+
+Scenario(
+  'should request for original data after mocking stopped @Puppeteer',
+  (I) => {
+    I.amOnPage('/form/fetch_call');
+    I.click('GET COMMENTS');
+    I.waitForResponse(fetchComments);
+    I.mockRequest('GET', 'https://jsonplaceholder.typicode.com/*', {
+      comment: 'CUSTOM',
+    });
+    I.click('GET COMMENTS');
+    I.see('CUSTOM', '#data');
+    I.stopMocking();
+
+    I.click('GET COMMENTS');
+    I.waitForResponse(fetchComments);
+    I.dontSee('CUSTOM', '#data');
+  },
+);
