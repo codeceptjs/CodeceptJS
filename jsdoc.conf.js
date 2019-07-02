4dd772b387a88b325040ee798d55e0180a9b1b72
Lisa Martens <lemnis.martens@gmail.com>
2019-07-02 13:57:49 +0200

    JSDoc: include specific files & folders for definitions

--- jsdoc.conf.js
+++ jsdoc.conf.js
@@ -1,7 +1,19 @@
 module.exports = {
   source: {
-    includePattern: '.+\\.js(doc|x)?$',
-    excludePattern: '(^|\\/|\\\\)_',
+    include: [
+      './lib/actor.js',
+      './lib/codecept.js',
+      './lib/index.js',
+      './lib/locator.js',
+      './lib/pause.js',
+      './lib/secret.js',
+      './lib/session.js',
+      './lib/ui.js',
+      './lib/within.js',
+      './lib/data/context.js',
+      './lib/interfaces',
+      './docs/build',
+    ],
   },
   opts: {
     template: 'node_modules/tsd-jsdoc/dist',
