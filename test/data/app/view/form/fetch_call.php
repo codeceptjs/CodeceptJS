d11dcff2e94da09fb7095b70a84650bb1759d3b3
Vikalp <vikalppanseriya@gmail.com>
2019-07-07 06:30:06 +0530

    feat: Mocking network requests/response using PollyJS (#1733)
    * feat: add method to start mocking
    
    * feat: add method to mock requests
    
    * add test cases for I.startMocking & I.mock
    
    * refactor: abstract Polly setup
    
    * refactor: separate out Polly helper
    
    * refactor: add acceptance test for Polly
    
    * test: add more test cases for different scenarios
    
    * refactor: start mocking automatically
    
    fix: disconnect Puppeteer when stop mocking.
    
    * test: add test for stopMocking
    
    refactor: change testCases according to new method name I.mockRequest()
    
    * style: remove line-breaks
    
    * test: add timeout for waiting response
    
    * test: remove waiting for response
    
    * test: remove redundant package requirement
    
    * fix: set by default let pass-through for all requests
    
    * test: remove wait for response

---
+++ test/data/app/view/form/fetch_call.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Fetch JSON data</title>
+    <style>
+      td {
+        padding: 4px;
+        border: 1px solid #333333;
+      }
+    </style>
+  </head>
+  <body>
+    <h3>JSON data</h3>
+    <button onclick="getPostData()">GET POSTS</button>
+    <button onclick="getCommentsData()">GET COMMENTS</button>
+    <button onclick="getUsersData()">GET USERS</button>
+    <div id="data">
+      <h4>No data here</h4>
+    </div>
+  </body>
+
+  <script type="text/javascript">
+    const tableData = data =>
+      Object.entries(data).reduce(
+        (html, [key, value]) => `${html} 
+            <tr>
+              <td>${key}</td>
+              <td>${value}</td>
+            </tr>
+          `,
+        ""
+      );
+
+    const data = document.querySelector("#data");
+
+    const getData = url =>
+      fetch(url)
+        .then(response => response.json())
+        .then(json => {
+          data.innerHTML = `<table>
+            ${tableData(json)}
+          </table>`;
+
+          console.log(json);
+        })
+        .catch(() => {
+          data.innerHTML = "Can not load data!";
+        });
+
+    const getPostData = () =>
+      getData("https://jsonplaceholder.typicode.com/posts/1");
+    const getCommentsData = () =>
+      getData("https://jsonplaceholder.typicode.com/comments/1");
+    const getUsersData = () =>
+      getData("https://jsonplaceholder.typicode.com/users/1");
+  </script>
+</html>
