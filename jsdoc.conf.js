c15d2be16f4692ea54628ef80ff92a510f500dc8
Lisa Martens <lemnis.martens@gmail.com>
2019-07-17 00:58:36 +0200

    Update config files

--- jsdoc.conf.js
+++ jsdoc.conf.js
@@ -1,16 +1,17 @@
 module.exports = {
   source: {
     include: [
-      './lib/actor.js',
+      './lib/config.js',
       './lib/codecept.js',
-      './lib/index.js',
+      './lib/container.js',
       './lib/locator.js',
       './lib/pause.js',
       './lib/secret.js',
       './lib/session.js',
+      './lib/Helper.js',
       './lib/ui.js',
       './lib/within.js',
-      './lib/data/context.js',
+      './lib/data/table.js',
       './lib/interfaces',
       './docs/build',
     ],
