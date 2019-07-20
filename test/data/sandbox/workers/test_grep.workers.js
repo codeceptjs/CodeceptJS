60f2f6d776f0a87ff49f9c4215f648f7aaeb3352
VikalpP <vikalppanseriya@gmail.com>
2019-07-20 09:29:20 +0530

    test: refactor test content to remove clones
    style: remove line-breaks

--- test/data/sandbox/workers/test_grep.workers.js
+++ test/data/sandbox/workers/test_grep.workers.js
@@ -1,9 +1,9 @@
-Feature('@feature_grep');
+Feature('@feature_grep in worker');
 
-Scenario('@1_grep print message 1', (I) => {
-  console.log('grep message 1');
+Scenario('From worker @1_grep print message 1', (I) => {
+  console.log('message 1');
 });
 
-Scenario('@2_grep print message 2', (I) => {
-  console.log('grep message 2');
+Scenario('From worker @2_grep print message 2', (I) => {
+  console.log('message 2');
 });
