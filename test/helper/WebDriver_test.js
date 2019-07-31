81ed56e9bea79635f1bc721edcd482b7d288b6dd
PeterNgTr <peter.nguyentr@gmail.com>
2019-07-31 15:25:15 +0200

    reword to grabGeoLocation to match general style

--- test/helper/WebDriver_test.js
+++ test/helper/WebDriver_test.js
@@ -734,7 +734,7 @@ describe('WebDriver', function () {
   describe('GeoLocation', () => {
     it('should set the geoLocation', async () => {
       await wd.setGeoLocation(37.4043, -122.0748);
-      const geoLocation = await wd.getGeoLocation();
+      const geoLocation = await wd.grabGeoLocation();
       assert.equal(geoLocation.latitude, 37.4043, 'The latitude is not properly set');
       assert.equal(geoLocation.longitude, -122.0748, 'The longitude is not properly set');
     });
