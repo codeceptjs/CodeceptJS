4ca12e6f09454031d863d937d0d100b66f625475
Evgeniy Lukoyanov <11647141+elukoyanov@users.noreply.github.com>
2019-08-12 12:56:25 +0300

    update mocha to 6 (#1802)
    * update mocha to 6
    
    * Update package.json
    
    Change mocha version to 6.2.0+
    
    Co-Authored-By: Vikalp <vikalppanseriya@gmail.com>
    
    * remove co-mocha and remove generators from tests
    
    * make webapi, rest and webdriver tests use await
    
    * make protractor, nightmare, appium tests use await
    
    * make webdriver use await
    
    * make webdriverio and puppeteer tests use await
    
    * fix screenshotOnFail plugin. update mocha imports
    
    * fix linter errors

--- test/helper/AppiumWeb_test.js
+++ test/helper/AppiumWeb_test.js
@@ -1,19 +1,7 @@
 const Appium = require('../../lib/helper/Appium');
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
 
-chai.use(chaiAsPromised);
-const expect = chai.expect;
 let I;
 const site_url = 'http://davertmik.github.io';
-const assert = require('assert');
-const path = require('path');
-const fs = require('fs');
-const fileExists = require('../../lib/utils').fileExists;
-const AssertionFailedError = require('../../lib/assert/error');
-const webApiTests = require('./webapi');
-const within = require('../../lib/within');
-require('co-mocha')(require('mocha'));
 
 describe('Appium Web', function () {
   this.retries(4);
@@ -54,121 +42,121 @@ describe('Appium Web', function () {
   afterEach(() => I._after());
 
   describe('current url : #seeInCurrentUrl, #seeCurrentUrlEquals, ...', () => {
-    it('should check for url fragment', function* () {
-      yield I.amOnPage('/angular-demo-app/#/info');
-      yield I.seeInCurrentUrl('/info');
-      return I.dontSeeInCurrentUrl('/result');
+    it('should check for url fragment', async () => {
+      await I.amOnPage('/angular-demo-app/#/info');
+      await I.seeInCurrentUrl('/info');
+      await I.dontSeeInCurrentUrl('/result');
     });
 
-    it('should check for equality', function* () {
-      yield I.amOnPage('/angular-demo-app/#/info');
-      yield I.seeCurrentUrlEquals('/angular-demo-app/#/info');
-      return I.dontSeeCurrentUrlEquals('/angular-demo-app/#/result');
+    it('should check for equality', async () => {
+      await I.amOnPage('/angular-demo-app/#/info');
+      await I.seeCurrentUrlEquals('/angular-demo-app/#/info');
+      await I.dontSeeCurrentUrlEquals('/angular-demo-app/#/result');
     });
   });
 
   describe('see text : #see', () => {
-    it('should check text on site', function* () {
-      yield I.amOnPage('/angular-demo-app/');
-      yield I.see('Description');
-      return I.dontSee('Create Event Today');
+    it('should check text on site', async () => {
+      await I.amOnPage('/angular-demo-app/');
+      await I.see('Description');
+      await I.dontSee('Create Event Today');
     });
 
-    it('should check text inside element', function* () {
-      yield I.amOnPage('/angular-demo-app/#/info');
-      yield I.see('About', 'h1');
-      yield I.see('Welcome to event app', { css: 'p.jumbotron' });
-      return I.see('Back to form', '//div/a');
+    it('should check text inside element', async () => {
+      await I.amOnPage('/angular-demo-app/#/info');
+      await I.see('About', 'h1');
+      await I.see('Welcome to event app', { css: 'p.jumbotron' });
+      await I.see('Back to form', '//div/a');
     });
   });
 
   describe('see element : #seeElement, #dontSeeElement', () => {
-    it('should check visible elements on page', function* () {
-      yield I.amOnPage('/angular-demo-app/');
-      yield I.seeElement('.btn.btn-primary');
-      yield I.seeElement({ css: '.btn.btn-primary' });
-      return I.dontSeeElement({ css: '.btn.btn-secondary' });
+    it('should check visible elements on page', async () => {
+      await I.amOnPage('/angular-demo-app/');
+      await I.seeElement('.btn.btn-primary');
+      await I.seeElement({ css: '.btn.btn-primary' });
+      await I.dontSeeElement({ css: '.btn.btn-secondary' });
     });
   });
 
 
   describe('#click', () => {
-    it('should click by text', function* () {
-      yield I.amOnPage('/angular-demo-app/');
-      yield I.dontSeeInCurrentUrl('/info');
-      yield I.click('Get more info!');
-      return I.seeInCurrentUrl('/info');
+    it('should click by text', async () => {
+      await I.amOnPage('/angular-demo-app/');
+      await I.dontSeeInCurrentUrl('/info');
+      await I.click('Get more info!');
+      await I.seeInCurrentUrl('/info');
     });
 
-    it('should click by css', function* () {
-      yield I.amOnPage('/angular-demo-app/');
-      yield I.click('.btn-primary');
-      yield I.wait(2);
-      return I.seeInCurrentUrl('/result');
+    it('should click by css', async () => {
+      await I.amOnPage('/angular-demo-app/');
+      await I.click('.btn-primary');
+      await I.wait(2);
+      await I.seeInCurrentUrl('/result');
     });
 
-    it('should click by non-optimal css', function* () {
-      yield I.amOnPage('/angular-demo-app/');
-      yield I.click('form a.btn');
-      yield I.wait(2);
-      return I.seeInCurrentUrl('/result');
+    it('should click by non-optimal css', async () => {
+      await I.amOnPage('/angular-demo-app/');
+      await I.click('form a.btn');
+      await I.wait(2);
+      await I.seeInCurrentUrl('/result');
     });
 
-    it('should click by xpath', function* () {
-      yield I.amOnPage('/angular-demo-app/');
-      yield I.click('//a[contains(., "more info")]');
-      return I.seeInCurrentUrl('/info');
+    it('should click by xpath', async () => {
+      await I.amOnPage('/angular-demo-app/');
+      await I.click('//a[contains(., "more info")]');
+      await I.seeInCurrentUrl('/info');
     });
 
-    it('should click on context', function* () {
-      yield I.amOnPage('/angular-demo-app/');
-      yield I.click('.btn-primary', 'form');
-      yield I.wait(2);
-      return I.seeInCurrentUrl('/result');
+    it('should click on context', async () => {
+      await I.amOnPage('/angular-demo-app/');
+      await I.click('.btn-primary', 'form');
+      await I.wait(2);
+      await I.seeInCurrentUrl('/result');
     });
 
-    it('should click link with inner span', function* () {
-      yield I.amOnPage('/angular-demo-app/#/result');
-      yield I.click('Go to info');
-      return I.seeInCurrentUrl('/info');
+    it('should click link with inner span', async () => {
+      await I.amOnPage('/angular-demo-app/#/result');
+      await I.click('Go to info');
+      await I.seeInCurrentUrl('/info');
     });
 
-    it('should click buttons as links', function* () {
-      yield I.amOnPage('/angular-demo-app/');
-      yield I.click('Options');
-      return I.seeInCurrentUrl('/options');
+    it('should click buttons as links', async () => {
+      await I.amOnPage('/angular-demo-app/');
+      await I.click('Options');
+      await I.seeInCurrentUrl('/options');
     });
   });
 
   describe('#grabTextFrom, #grabValueFrom, #grabAttributeFrom', () => {
-    it('should grab text from page', function* () {
-      yield I.amOnPage('/angular-demo-app/#/info');
-      const val = yield I.grabTextFrom('p.jumbotron');
-      return expect(val).to.equal('Welcome to event app');
+    it('should grab text from page', async () => {
+      await I.amOnPage('/angular-demo-app/#/info');
+      const val = await I.grabTextFrom('p.jumbotron');
+      val.should.be.equal('Welcome to event app');
     });
 
-    it('should grab value from field', function* () {
-      yield I.amOnPage('/angular-demo-app/#/options');
-      const val = yield I.grabValueFrom('#ssh');
-      return expect(val).to.equal('PUBLIC-SSH-KEY');
+    it('should grab value from field', async () => {
+      await I.amOnPage('/angular-demo-app/#/options');
+      const val = await I.grabValueFrom('#ssh');
+      val.should.be.equal('PUBLIC-SSH-KEY');
     });
 
-    it('should grab attribute from element', function* () {
-      yield I.amOnPage('/angular-demo-app/#/info');
-      const val = yield I.grabAttributeFrom('a.btn', 'ng-href');
-      return expect(val).to.equal('#/');
+    it('should grab attribute from element', async () => {
+      await I.amOnPage('/angular-demo-app/#/info');
+      const val = await I.grabAttributeFrom('a.btn', 'ng-href');
+      val.should.be.equal('#/');
     });
   });
 
   describe('#within', () => {
     afterEach(() => I._withinEnd());
 
-    it('should work using within operator', function* () {
-      yield I.amOnPage('/angular-demo-app/#/options');
-      yield I.see('Choose if you ok with terms');
-      yield I._withinBegin({ css: 'div.results' });
-      yield I.see('SSH Public Key: PUBLIC-SSH-KEY');
-      return I.dontSee('Options');
+    it('should work using within operator', async () => {
+      await I.amOnPage('/angular-demo-app/#/options');
+      await I.see('Choose if you ok with terms');
+      await I._withinBegin({ css: 'div.results' });
+      await I.see('SSH Public Key: PUBLIC-SSH-KEY');
+      await I.dontSee('Options');
     });
   });
 });
