f2c10dac8bb467180915072be4a0fd3752f75604
Radheshyam Etikala <radhey1851@gmail.com>
2019-08-15 23:19:30 +0530

    Add scripts to setup polly with xhr in PollyWd.js and add setup to Polly helper

--- test/acceptance/codecept.WebDriverIO.js
+++ test/acceptance/codecept.WebDriverIO.js
@@ -17,7 +17,7 @@ module.exports.config = {
       //   },
       // },
     },
-
+    Polly: {},
   },
   include: {},
   bootstrap: done => setTimeout(done, 5000), // let's wait for selenium
@@ -26,5 +26,11 @@ module.exports.config = {
   gherkin: {
     features: './gherkin/*.feature',
     steps: ['./gherkin/steps.js'],
+  },
+  plugins: {
+    wdio: {
+      enabled: true,
+      services: ['selenium-standalone'],
+    },
   },
 };
