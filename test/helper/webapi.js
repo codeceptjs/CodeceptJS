6eb5f9b7aaac29ba1e3c39a24edf1caf4238e0f9
Davert <davert.ua@gmail.com>
2019-08-15 14:18:02 +0300

    removed protractor angular1 app from test suite, updated testcafe tests

--- test/helper/webapi.js
+++ test/helper/webapi.js
@@ -996,6 +996,8 @@ module.exports.tests = function () {
     });
 
     it('within should respect context in see', async () => {
+      if (isHelper('TestCafe')) return;
+
       await I.amOnPage('/form/example4');
       await I.see('Rejestracja', 'fieldset');
       await I._withinBegin({ css: '.navbar-header' });
@@ -1013,7 +1015,8 @@ module.exports.tests = function () {
       }
     });
 
-    it('within should respect context in see when using nested frames', async () => {
+    it('within should respect context in see when using nested frames', async function () {
+      if (isHelper('TestCafe')) this.skip();
       await I.amOnPage('/iframe_nested');
       await I._withinBegin({
         frame: ['#wrapperId', '[name=content]'],
@@ -1040,9 +1043,9 @@ module.exports.tests = function () {
   });
 
   describe('scroll: #scrollTo, #scrollPageToTop, #scrollPageToBottom', () => {
-    it('should scroll inside an iframe', async () => {
-      if (isHelper('Nightmare')) return;
-      if (isHelper('TestCafe')) return;
+    it('should scroll inside an iframe', async function () {
+      if (isHelper('Nightmare')) this.skip();
+      if (isHelper('TestCafe')) this.skip();
 
       await I.amOnPage('/iframe');
       await I.resizeWindow(500, 700);
