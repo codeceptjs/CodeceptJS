60a4967cdfadc563871009ceccbe86311ee54817
Michael Bodnarchuk <DavertMik@users.noreply.github.com>
2019-09-24 03:42:20 +0300

    Improved retryFailedStep plugin (#1897)
    * improved retryFailedStep plugin
    
    * fixed tests
    
    * added custom config for ignored steps
    
    * fixed formatting

--- test/data/sandbox/retry_helper.js
+++ test/data/sandbox/retry_helper.js
@@ -4,6 +4,10 @@ class Retry extends Helper {
   failWhen(fn) {
     if (fn()) throw new Error('ups, error');
   }
+
+  waitForFail(fn) {
+    if (fn()) throw new Error('ups, error');
+  }
 }
 
 module.exports = Retry;
