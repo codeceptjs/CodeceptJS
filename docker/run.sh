5083df38aab643ca92d1f4a6c76ebe2353259ba1
PeterNgTr <peter.nguyentr@gmail.com>
2019-10-02 11:03:25 +0200

    improve run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -4,22 +4,24 @@ source /codecept/docker/help.sh
 
 # Check if tests are correctly mounted
 if [[ -d "/tests/" ]]; then
-	echo "CodeceptJS directory has been found."
+        echo "CodeceptJS directory has been found."
 
 	# Run the tests
-	cd /tests/
+	cd /tests/ || exit
 	if [ "$RUN_MULTIPLE" = true ]; then
 		echo "Tests are split into chunks and executed in multiple processes."
 		if [ ! "$CODECEPT_ARGS" ]; then
 			echo "No CODECEPT_ARGS provided. Tests will procceed with --all option to run all configured runs"
 			codeceptjs run-multiple --all
 		else		
-			codeceptjs run-multiple $CODECEPT_ARGS
+			codeceptjs run-multiple "$CODECEPT_ARGS"
 		fi
-	else if [ "$NO_OF_WORKERS" > 0 ]; then
-		codeceptjs run-workers $NO_OF_WORKERS
 	else
-		codeceptjs run $CODECEPT_ARGS
+		if [ "$NO_OF_WORKERS" -gt 0 ]; then
+		        codeceptjs run-workers "$NO_OF_WORKERS"
+		else
+		        codeceptjs run "$CODECEPT_ARGS"
+		fi
 	fi
 else
 	display_usage
