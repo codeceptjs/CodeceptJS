8cea1736055f1441033aa6330f7db411357ec019
Alexandr Vorobey <sashavorobey95@gmail.com>
2020-02-12 00:49:56 +0300

    fix(Puppeteer): #waitForText - Cannot read property 'innerText' of null (#2181)
    * fix(Puppeteer): #waitForText - Cannot read property 'innerText' of null
    
    * fix comment
    
    * fix comments

--- test/helper/Puppeteer_test.js
+++ test/helper/Puppeteer_test.js
@@ -653,6 +653,13 @@ describe('Puppeteer', function () {
       .then(() => I.see('button was clicked', '#message')));
   });
 
+  describe('#waitForText', () => {
+    it('should wait for text after load body', async () => {
+      await I.amOnPage('/redirect_long');
+      await I.waitForText('Hi there and greetings!', 5);
+    });
+  });
+
   describe('#waitForValue', () => {
     it('should wait for expected value for given locator', () => I.amOnPage('/info')
       .then(() => I.waitForValue('//input[@name= "rus"]', 'Верно'))
