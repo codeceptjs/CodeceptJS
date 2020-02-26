e5630e8fc17bee86c2d41123ca30288bb3840b77
Koushik Mohan <koushikmohan1996@gmail.com>
2020-02-26 10:11:10 +0000

    Feat: Add public API for workers (#2115)
    * Feat: Add public API for workers
    
    * Add boostrab and teardown hooks
    
    * Refract worker and Add tests
    
    * Add tests and Refract worker
    
    * Remove node 8 support
    
    * Add share API
    
    * Add share API review changes

---
+++ test/data/sandbox/custom-worker/share_test.worker.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+Feature('Shared Memory in Workers');
+
+Scenario('Should get the data shared from main process', (I) => {
+  I.say('Hello Workers');
+  const { fromMain } = inject();
+  console.log(fromMain);
+  assert.equal(fromMain, true);
+});
+
+Scenario('Should get the data shared from other worker', (I) => {
+  I.amInPath('.');
+  I.say('hello world');
+  const { fromWorker } = inject();
+  assert.equal(fromWorker, true);
+});
