8ffb5c9fb4cac126ca18f89d8c29a68a35acb1d8
Koushik Mohan <koushikmohan1996@gmail.com>
2020-02-26 10:15:21 +0000

    feat : Add grabCssPropertyFrom method for Nightmare (#2111)
    * Add grabCssPropertyFrom method for Nightmare
    
    * Fix Doc comment
    
    * Change to for of loop

--- test/helper/webapi.js
+++ test/helper/webapi.js
@@ -1138,7 +1138,6 @@ module.exports.tests = function () {
 
   describe('#grabCssPropertyFrom', () => {
     it('should grab css property for given element', async function () {
-      if (isHelper('Nightmare')) return;
       if (isHelper('TestCafe')) this.skip();
 
       await I.amOnPage('/form/doubleclick');
@@ -1147,7 +1146,6 @@ module.exports.tests = function () {
     });
 
     it('should grab camelcased css properies', async () => {
-      if (isHelper('Nightmare')) return;
       if (isHelper('TestCafe')) return;
 
       await I.amOnPage('/form/doubleclick');
