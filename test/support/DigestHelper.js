8fb08d41c1030000575ade470f86e9f0779b187d
Andrew Lehman <alehman@edc.nyc>
2020-03-09 13:31:15 -0400

    Create Digest helper
    Added a helper for helper tests that will create an MD5 digest from a
    path to a file.  The point of this helper is to aid in determining if
    screenshots taken in different sessions within Puppeteer are actually
    different or the same.

---
+++ test/support/DigestHelper.js
@@ -0,0 +1,14 @@
+const crypto = require('crypto');
+const fs = require('fs');
+
+class DigestHelper {
+  getMD5Digest(filePath) {
+    const hash = crypto.createHash('md5');
+    const data = fs.readFileSync(filePath);
+    hash.update(data);
+
+    return hash.digest('base64');
+  }
+}
+
+module.exports = DigestHelper;
