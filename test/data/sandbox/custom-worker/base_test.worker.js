99771f0d4ace153e965250adf548e805b8a405c1
Vorobeyko <sashavorobey95@gmail.com>
2020-04-08 01:10:35 +0300

    fix workers unit tests

--- test/data/sandbox/custom-worker/base_test.worker.js
+++ test/data/sandbox/custom-worker/base_test.worker.js
@@ -1,11 +1,11 @@
 Feature('Workers');
 
-Scenario('say something', (I) => {
+Scenario('say something', ({ I }) => {
   I.say('Hello Workers');
   share({ fromWorker: true });
 });
 
-Scenario('glob current dir', (I) => {
+Scenario('glob current dir', ({ I }) => {
   I.amInPath('.');
   I.say('hello world');
   I.seeFile('codecept.glob.json');
