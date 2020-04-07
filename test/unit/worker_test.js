cb5ae8793df0b8285944fcd632b357199ba735ac
Vorobeyko <sashavorobey95@gmail.com>
2020-04-08 02:06:21 +0300

    return

--- test/unit/worker_test.js
+++ test/unit/worker_test.js
@@ -2,9 +2,8 @@ const { expect } = require('chai');
 const path = require('path');
 const semver = require('semver');
 
+
 describe('Workers', () => {
-  if (!semver.satisfies(process.version, '>=11.7.0')) return;
-  const { Workers, event } = require('../../lib/index');
   before(() => {
     global.codecept_dir = path.join(__dirname, '/../data/sandbox');
   });
