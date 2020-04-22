354a8ecff83eb28df113d07f06b3c919a19bb247
Davert <davert.ua@gmail.com>
2020-04-23 00:47:38 +0300

    attachFile was fixed

--- test/helper/webapi.js
+++ test/helper/webapi.js
@@ -649,9 +649,6 @@ module.exports.tests = function () {
   });
 
   describe('#attachFile', () => {
-    // TODO: Those tests are skipped due to bug https://github.com/puppeteer/puppeteer/issues/5543
-    if (isHelper('Puppeteer')) this.skip();
-
     it('should upload file located by CSS', async () => {
       await I.amOnPage('/form/file');
       await I.attachFile('#avatar', 'app/avatar.jpg');
