d870ab5d64166a42c2628270e03d7ab9926b6b1d
Alexandr Vorobey <sashavorobey95@gmail.com>
2020-04-25 10:50:58 +0300

    Improve Scenario DX (#2231)
    * Improve Scenario DX #1972
    
    * add migrate script
    
    * add data set scenario test
    
    * Update utils.js
    
    * Update ddt_test.addt.js
    
    * Update interface_test.js
    
    * Update scenario.js
    
    * fix tests with new args transformation
    
    * remove only leftover
    
    * remove only leftover
    
    * remove --verbose leftover
    
    * fix
    
    * fix workers unit tests
    
    * remove unnecessary transform files
    
    * fix DX
    
    * fix runner tests
    
    * run github action tests on pr
    
    * not run workers tests on 10 node
    
    * return
    
    * fix
    
    * add new typings
    
    * fix lints
    
    * fix pr
    
    * fix husky
    
    * fix github actions
    
    * fix github actions
    
    * fix github actions
    
    * fix
    
    * fix
    
    Co-authored-by: Peter Nguyen Tr <peter.nguyentr@gmail.com>

--- test/data/sandbox/configs/allure/before_suite_test_failed.js
+++ test/data/sandbox/configs/allure/before_suite_test_failed.js
@@ -4,10 +4,10 @@ BeforeSuite(async () => {
   throw new Error('the before suite setup failed');
 });
 
-Scenario('failing setup test 1', async I => {
+Scenario('failing setup test 1', async ({ I }) => {
   I.say('Test was fine.');
 });
 
-Scenario('failing setup test 2', async I => {
+Scenario('failing setup test 2', async ({ I }) => {
   I.say('Test was fine.');
 });
--- test/data/sandbox/custom-worker/share_test.worker.js
+++ test/data/sandbox/custom-worker/share_test.worker.js
@@ -2,14 +2,14 @@ const assert = require('assert');
 
 Feature('Shared Memory in Workers');
 
-Scenario('Should get the data shared from main process', (I) => {
+Scenario('Should get the data shared from main process', ({ I }) => {
   I.say('Hello Workers');
   const { fromMain } = inject();
   console.log(fromMain);
   assert.equal(fromMain, true);
 });
 
-Scenario('Should get the data shared from other worker', (I) => {
+Scenario('Should get the data shared from other worker', ({ I }) => {
   I.amInPath('.');
   I.say('hello world');
   const { fromWorker } = inject();
