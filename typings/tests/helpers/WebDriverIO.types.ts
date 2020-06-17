0e4ae9c7ab67e5cb6d5bafb70ad13d1672ea59a4
Vorobeyko <sashavorobey95@gmail.com>
2020-06-18 00:54:04 +0300

    f

--- typings/tests/helpers/WebDriverIO.types.ts
+++ typings/tests/helpers/WebDriverIO.types.ts
@@ -1,3 +1,4 @@
-Scenario('', ({ I }) => {
-  I.grabAllWindowHandles() // $ExpectType string[]
-})
+const wd = new CodeceptJS.WebDriver()
+
+wd.grabAllWindowHandles() // $ExpectType string[]
+wd.grabCurrentWindowHandle() // $ExpectType string
