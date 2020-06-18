8fc43239b433e7006ae22057de3d3e97e5a675c6
Alexandr Vorobey <sashavorobey95@gmail.com>
2020-06-19 00:04:14 +0300

    fix(po): failed metastep with context (#2442)
    * fix(po): failed metastep with context
    
    * add test
    
    * fix

--- test/data/sandbox/configs/pageObjects/fs_test.inject.po.js
+++ test/data/sandbox/configs/pageObjects/fs_test.inject.po.js
@@ -9,3 +9,11 @@ Scenario('check current dir', () => {
   MyPage.hasFile('uu');
   I.seeFile('codecept.po.json');
 });
+
+Scenario('pageobject with context', async ({ I, MyPage, SecondPage }) => {
+  I.openDir('aaa');
+  I.seeFile('codecept.class.js');
+  MyPage.hasFile('uu');
+  I.seeFile('codecept.po.json');
+  await SecondPage.assertLocator();
+});
