c32126c94665ec1cd8cc126b0380a9fd2a770abd
suniljaiswal01 <sunilj271@gmail.com>
2020-07-21 19:02:16 +0530

    Added `forceRightClick` method to emulate rightclick event instead of using native events.

--- test/helper/WebDriver_test.js
+++ test/helper/WebDriver_test.js
@@ -165,6 +165,29 @@ describe('WebDriver', function () {
     });
   });
 
+  describe('Force Right Click: #forceRightClick', () => {
+    it('it should forceRightClick', async () => {
+      await wd.amOnPage('/form/rightclick');
+      await wd.dontSee('right clicked');
+      await wd.forceRightClick('Lorem Ipsum');
+      await wd.see('right clicked');
+    });
+
+    it('it should forceRightClick by locator', async () => {
+      await wd.amOnPage('/form/rightclick');
+      await wd.dontSee('right clicked');
+      await wd.forceRightClick('.context a');
+      await wd.see('right clicked');
+    });
+
+    it('it should forceRightClick by locator and context', async () => {
+      await wd.amOnPage('/form/rightclick');
+      await wd.dontSee('right clicked');
+      await wd.forceRightClick('Lorem Ipsum', '.context');
+      await wd.see('right clicked');
+    });
+  });
+  
   describe('#pressKey, #pressKeyDown, #pressKeyUp', () => {
     it('should be able to send special keys to element', async () => {
       await wd.amOnPage('/form/field');
