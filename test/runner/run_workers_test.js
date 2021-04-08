ffeb83db505b7e1a07cf6c2638b668f280ff7197
a.gulbe <a.gulbe@tinkoff.ru>
2021-04-08 19:46:53 +0300

    rerun ci

--- test/runner/run_workers_test.js
+++ test/runner/run_workers_test.js
@@ -199,7 +199,6 @@ describe('CodeceptJS Workers Runner', function () {
       expect(stderr).not.toContain('UnhandledPromiseRejectionWarning');
       expect(stdout).toContain('badFn is not defined');
       expect(err).not.toBe(null);
-
       done();
     });
   });
