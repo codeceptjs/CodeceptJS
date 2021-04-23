d3174f3412320961ab58a6b44b0c18d5b569f53c
Peter Ng <peter.nguyentr@gmail.com>
2021-04-23 22:57:02 +0200

    feat: versioning automatically (#2883)
    * feat: versioning automatically
    
    * chore: remove redundant log

--- runok.js
+++ runok.js
@@ -390,6 +390,26 @@ title: ${name}
     console.log('-- RELEASED --');
   },
 
+  async versioning() {
+    const semver = require('semver');
+
+    if (fs.existsSync('./package.json')) {
+      const packageFile = require('./package.json');
+      const currentVersion = packageFile.version;
+      let type = process.argv[3];
+      if (!['major', 'minor', 'patch'].includes(type)) {
+        type = 'patch';
+      }
+
+      const newVersion = semver.inc(packageFile.version, type);
+      packageFile.version = newVersion;
+      fs.writeFileSync('./package.json', JSON.stringify(packageFile, null, 2));
+      console.log('Version updated', currentVersion, '=>', newVersion);
+
+      console.log('Creating and switching to release branch...');
+      await exec(`git checkout -b release-${newVersion}`);
+    }
+  },
 };
 
 async function processChangelog() {
