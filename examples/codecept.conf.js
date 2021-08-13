2e98df8420c76335304551c87731cc0c9f4ce72f
Anirudh Modi <anirudh.modi2@gmail.com>
2021-08-13 20:53:09 +0530

    update: revert changes to example config, plugin revert

--- examples/codecept.conf.js
+++ examples/codecept.conf.js
@@ -10,7 +10,6 @@ exports.config = {
         // browserWSEndpoint: 'ws://127.0.0.1:45635/09b7aa1ac28c317e5abee7cb6d35d519',
       },
       show: !process.env.HEADLESS,
-      video: true,
     },
     REST: {},
   },
@@ -34,9 +33,6 @@ exports.config = {
   plugins: {
     tryTo: {
       enabled: true,
-    },
-    captureStepAsSubtitle: {
-      enabled: false,
     },
     allure: {
       enabled: false,
