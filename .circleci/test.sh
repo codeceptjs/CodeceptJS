0ede3024812a1ac22cda09015b83df083f586ae4
PeterNgTr <peter.nguyentr@gmail.com>
2021-10-13 09:00:45 +0200

    CR addressed

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -6,4 +6,6 @@ cd test
 
 docker-compose run --rm test-unit &&
 docker-compose run --rm test-rest &&
-docker-compose run --rm test-acceptance.nightmare
+docker-compose run --rm test-acceptance.webdriverio &&
+docker-compose run --rm test-acceptance.nightmare &&
+docker-compose run --rm test-acceptance.puppeteer
