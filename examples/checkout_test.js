9a809ef7fce07a0b58e29d9a1a95e367466f7cfa
Davert <davert.ua@gmail.com>
2021-11-23 01:30:58 +0200

    updated examples & docs

--- examples/checkout_test.js
+++ examples/checkout_test.js
@@ -4,13 +4,19 @@ Before(({ I }) => {
   I.amOnPage('https://getbootstrap.com/docs/4.0/examples/checkout/');
 });
 
-Scenario('It should fill in checkout page', ({ I }) => {
+Scenario('It should fill in checkout page', async ({ I }) => {
   I.fillField('#lastName', 'mik');
-  I.fillField('Promo code', '123345');
-  I.click('Redeem');
-  I.click(locate('h6').withText('Third item'));
-  I.checkOption('#same-address');
-  I.see('Paymen1t');
+  await retryTo((retryNum) => {
+    // I.usePlaywrightTo('set 0', async ({ browserContext }) => {
+    //   await browserContext.setDefaultTimeout(0);
+    // });
+    console.log('retry', retryNum);
+    I.fillField('Promo code', '123345');
+    I.click('Redeem');
+    I.click(locate('h6').withText('Third item'));
+    I.checkOption('#same-address');
+    I.see('Paymen1t');
+  }, 3);
   I.click('Paypal');
   I.click('Checkout');
   I.see('Thank you!');
