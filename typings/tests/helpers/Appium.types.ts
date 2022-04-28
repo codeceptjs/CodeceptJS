6c9cfca7af82bc07afe791c2671263f825905563
Davert <davert.ua@gmail.com>
2022-04-29 00:33:33 +0300

    fixed typings

--- typings/tests/helpers/Appium.types.ts
+++ typings/tests/helpers/Appium.types.ts
@@ -5,70 +5,70 @@ const num_ap = 1;
 
 appium.touchPerform(); // $ExpectError
 appium.touchPerform("press"); // $ExpectError
-appium.touchPerform([{ action: "press" }]); // $ExpectType Promise<any>
-appium.touchPerform([{ action: "press" }, { action: "release" }]); // $ExpectType Promise<any>
+appium.touchPerform([{ action: "press" }]); // $ExpectType void
+appium.touchPerform([{ action: "press" }, { action: "release" }]); // $ExpectType void
 appium.touchPerform([{ action: "press" }], [{ action: "release" }]); // $ExpectError
 
-appium.hideDeviceKeyboard(); // $ExpectType Promise<any>
-appium.hideDeviceKeyboard("tapOutside"); // $ExpectType Promise<any>
-appium.hideDeviceKeyboard("pressKey", "Done"); // $ExpectType Promise<any>
+appium.hideDeviceKeyboard(); // $ExpectType void
+appium.hideDeviceKeyboard("tapOutside"); // $ExpectType void
+appium.hideDeviceKeyboard("pressKey", "Done"); // $ExpectType void
 appium.hideDeviceKeyboard("pressKey", "Done", "Done"); // $ExpectError
 
 appium.removeApp(); // $ExpectError
-appium.removeApp("appName"); // $ExpectType Promise<any>
-appium.removeApp("appName", "com.example.android.apis"); // $ExpectType Promise<any>
+appium.removeApp("appName"); // $ExpectType void
+appium.removeApp("appName", "com.example.android.apis"); // $ExpectType void
 appium.removeApp("appName", "com.example.android.apis", "remove"); // $ExpectError
 
-appium.runOnIOS(str_ap, () => {}); // $ExpectType Promise<any>
-appium.runOnAndroid(str_ap, () => {}); // $ExpectType Promise<any>
+appium.runOnIOS(str_ap, () => {}); // $ExpectType void
+appium.runOnAndroid(str_ap, () => {}); // $ExpectType void
 appium.seeAppIsInstalled(str_ap); // $ExpectType Promise<void>
 appium.seeAppIsNotInstalled(str_ap); // $ExpectType Promise<void>
 appium.installApp(str_ap); // $ExpectType Promise<void>
-appium.removeApp(str_ap); // $ExpectType Promise<any>
+appium.removeApp(str_ap); // $ExpectType void
 appium.seeCurrentActivityIs(str_ap); // $ExpectType Promise<void>
 appium.seeDeviceIsLocked(); // $ExpectType Promise<void>
 appium.seeDeviceIsUnlocked(); // $ExpectType Promise<void>
 appium.seeOrientationIs("LANDSCAPE"); // $ExpectType Promise<void>
-appium.setOrientation("LANDSCAPE"); // $ExpectType Promise<any>
+appium.setOrientation("LANDSCAPE"); // $ExpectType void
 appium.grabAllContexts(); // $ExpectType Promise<string[]>
 appium.grabContext(); // $ExpectType Promise<string | null>
 appium.grabCurrentActivity(); // $ExpectType Promise<string>
 appium.grabNetworkConnection(); // $ExpectType Promise<{}>
 appium.grabOrientation(); // $ExpectType Promise<string>
 appium.grabSettings(); // $ExpectType Promise<string>
-appium._switchToContext(str_ap); // $ExpectType Promise<any>
+appium._switchToContext(str_ap); // $ExpectType void
 appium.switchToWeb(); // $ExpectType Promise<void>
 appium.switchToNative(); // $ExpectType Promise<void>
 appium.switchToNative(str_ap); // $ExpectType Promise<void>
 appium.startActivity(); // $ExpectType Promise<void>
 appium.setNetworkConnection(); // $ExpectType Promise<{}>
-appium.setSettings(str_ap); // $ExpectType Promise<any>
-appium.hideDeviceKeyboard(); // $ExpectType Promise<any>
+appium.setSettings(str_ap); // $ExpectType void
+appium.hideDeviceKeyboard(); // $ExpectType void
 appium.sendDeviceKeyEvent(num_ap); // $ExpectType Promise<void>
 appium.openNotifications(); // $ExpectType Promise<void>
 appium.makeTouchAction(); // $ExpectType Promise<void>
 appium.tap(str_ap); // $ExpectType Promise<void>
-appium.performSwipe(str_ap, str_ap); // $ExpectType Promise<any>
+appium.performSwipe(str_ap, str_ap); // $ExpectType void
 appium.swipeDown(str_ap); // $ExpectType Promise<void>
 appium.swipeLeft(str_ap); // $ExpectType Promise<void>
 appium.swipeRight(str_ap); // $ExpectType Promise<void>
 appium.swipeUp(str_ap); // $ExpectType Promise<void>
 appium.swipeTo(str_ap, str_ap, str_ap, num_ap, num_ap, num_ap); // $ExpectType Promise<void>
-appium.touchPerform([]); // $ExpectType Promise<any>
+appium.touchPerform([]); // $ExpectType void
 appium.pullFile(str_ap, str_ap); // $ExpectType Promise<string>
 appium.shakeDevice(); // $ExpectType Promise<void>
 appium.rotate(); // $ExpectType Promise<void>
 appium.setImmediateValue(); // $ExpectType Promise<void>
 appium.simulateTouchId(); // $ExpectType Promise<void>
 appium.closeApp(); // $ExpectType Promise<void>
-appium.appendField(str_ap, str_ap); // $ExpectType Promise<any>
-appium.checkOption(str_ap); // $ExpectType Promise<any>
-appium.click(str_ap); // $ExpectType Promise<any>
-appium.dontSeeCheckboxIsChecked(str_ap); // $ExpectType Promise<any>
-appium.dontSeeElement(str_ap); // $ExpectType Promise<any>
-appium.dontSeeInField(str_ap, str_ap); // $ExpectType Promise<any>
-appium.dontSee(str_ap); // $ExpectType Promise<any>
-appium.fillField(str_ap, str_ap); // $ExpectType Promise<any>
+appium.appendField(str_ap, str_ap); // $ExpectType void
+appium.checkOption(str_ap); // $ExpectType void
+appium.click(str_ap); // $ExpectType void
+appium.dontSeeCheckboxIsChecked(str_ap); // $ExpectType void
+appium.dontSeeElement(str_ap); // $ExpectType void
+appium.dontSeeInField(str_ap, str_ap); // $ExpectType void
+appium.dontSee(str_ap); // $ExpectType void
+appium.fillField(str_ap, str_ap); // $ExpectType void
 appium.grabTextFromAll(str_ap); // $ExpectType Promise<string[]>
 appium.grabTextFrom(str_ap); // $ExpectType Promise<string>
 appium.grabNumberOfVisibleElements(str_ap); // $ExpectType Promise<number>
@@ -77,13 +77,13 @@ appium.grabAttributeFromAll(str_ap, str_ap); // $ExpectType Promise<string[]>
 appium.grabValueFromAll(str_ap); // $ExpectType Promise<string[]>
 appium.grabValueFrom(str_ap); // $ExpectType Promise<string>
 appium.saveScreenshot(str_ap); // $ExpectType Promise<void>
-appium.scrollIntoView(str_ap, {}); // $ExpectType Promise<any>
-appium.seeCheckboxIsChecked(str_ap); // $ExpectType Promise<any>
-appium.seeElement(str_ap); // $ExpectType Promise<any>
-appium.seeInField(str_ap, str_ap); // $ExpectType Promise<any>
-appium.see(str_ap); // $ExpectType Promise<any>
-appium.selectOption(str_ap, str_ap); // $ExpectType Promise<any>
-appium.waitForElement(str_ap); // $ExpectType Promise<any>
-appium.waitForVisible(str_ap); // $ExpectType Promise<any>
-appium.waitForInvisible(str_ap); // $ExpectType Promise<any>
-appium.waitForText(str_ap); // $ExpectType Promise<any>
+appium.scrollIntoView(str_ap, {}); // $ExpectType void
+appium.seeCheckboxIsChecked(str_ap); // $ExpectType void
+appium.seeElement(str_ap); // $ExpectType void
+appium.seeInField(str_ap, str_ap); // $ExpectType void
+appium.see(str_ap); // $ExpectType void
+appium.selectOption(str_ap, str_ap); // $ExpectType void
+appium.waitForElement(str_ap); // $ExpectType void
+appium.waitForVisible(str_ap); // $ExpectType void
+appium.waitForInvisible(str_ap); // $ExpectType void
+appium.waitForText(str_ap); // $ExpectType void
--- typings/tests/helpers/Playwright.types.ts
+++ typings/tests/helpers/Playwright.types.ts
@@ -6,90 +6,90 @@ const position = { x: 100, y: 200 };
 const sourcePosition = { x: 10, y: 20 };
 const targetPosition = { x: 20, y: 30 };
 
-playwright.usePlaywrightTo(str_pl, () => {}); // $ExpectType Promise<any>
-playwright.amAcceptingPopups(); // $ExpectType Promise<any>
-playwright.acceptPopup(); // $ExpectType Promise<any>
-playwright.amCancellingPopups(); // $ExpectType Promise<any>
-playwright.cancelPopup(); // $ExpectType Promise<any>
-playwright.seeInPopup(str_pl); // $ExpectType Promise<any>
-playwright._setPage(str_pl); // $ExpectType Promise<any>
-playwright._addPopupListener(); // $ExpectType Promise<any>
-playwright._getPageUrl(); // $ExpectType Promise<any>
+playwright.usePlaywrightTo(str_pl, () => {}); // $ExpectType void
+playwright.amAcceptingPopups(); // $ExpectType void
+playwright.acceptPopup(); // $ExpectType void
+playwright.amCancellingPopups(); // $ExpectType void
+playwright.cancelPopup(); // $ExpectType void
+playwright.seeInPopup(str_pl); // $ExpectType void
+playwright._setPage(str_pl); // $ExpectType void
+playwright._addPopupListener(); // $ExpectType void
+playwright._getPageUrl(); // $ExpectType void
 playwright.grabPopupText(); // $ExpectType Promise<string | null>
-playwright.amOnPage(str_pl); // $ExpectType Promise<any>
-playwright.resizeWindow(num_pl, num_pl); // $ExpectType Promise<any>
-playwright.haveRequestHeaders(str_pl); // $ExpectType Promise<any>
-playwright.moveCursorTo(str_pl, num_pl, num_pl); // $ExpectType Promise<any>
+playwright.amOnPage(str_pl); // $ExpectType void
+playwright.resizeWindow(num_pl, num_pl); // $ExpectType void
+playwright.haveRequestHeaders(str_pl); // $ExpectType void
+playwright.moveCursorTo(str_pl, num_pl, num_pl); // $ExpectType void
 playwright.dragAndDrop(str_pl); // $ExpectError
-playwright.dragAndDrop(str_pl, str_pl); // $ExpectType Promise<any>
-playwright.dragAndDrop(str_pl, str_pl, { sourcePosition, targetPosition }); // $ExpectType Promise<any>
-playwright.refreshPage(); // $ExpectType Promise<any>
-playwright.scrollPageToTop(); // $ExpectType Promise<any>
-playwright.scrollPageToBottom(); // $ExpectType Promise<any>
-playwright.scrollTo(str_pl, num_pl, num_pl); // $ExpectType Promise<any>
-playwright.seeInTitle(str_pl); // $ExpectType Promise<any>
+playwright.dragAndDrop(str_pl, str_pl); // $ExpectType void
+playwright.dragAndDrop(str_pl, str_pl, { sourcePosition, targetPosition }); // $ExpectType void
+playwright.refreshPage(); // $ExpectType void
+playwright.scrollPageToTop(); // $ExpectType void
+playwright.scrollPageToBottom(); // $ExpectType void
+playwright.scrollTo(str_pl, num_pl, num_pl); // $ExpectType void
+playwright.seeInTitle(str_pl); // $ExpectType void
 playwright.grabPageScrollPosition(); // $ExpectType Promise<PageScrollPosition>
-playwright.seeTitleEquals(str_pl); // $ExpectType Promise<any>
-playwright.dontSeeInTitle(str_pl); // $ExpectType Promise<any>
+playwright.seeTitleEquals(str_pl); // $ExpectType void
+playwright.dontSeeInTitle(str_pl); // $ExpectType void
 playwright.grabTitle(); // $ExpectType Promise<string>
-playwright._locate(); // $ExpectType Promise<any>
-playwright._locateCheckable(); // $ExpectType Promise<any>
-playwright._locateClickable(); // $ExpectType Promise<any>
-playwright._locateFields(); // $ExpectType Promise<any>
-playwright.switchToNextTab(); // $ExpectType Promise<any>
-playwright.switchToPreviousTab(); // $ExpectType Promise<any>
-playwright.closeCurrentTab(); // $ExpectType Promise<any>
-playwright.closeOtherTabs(); // $ExpectType Promise<any>
-playwright.openNewTab(); // $ExpectType Promise<any>
+playwright._locate(); // $ExpectType void
+playwright._locateCheckable(); // $ExpectType void
+playwright._locateClickable(); // $ExpectType void
+playwright._locateFields(); // $ExpectType void
+playwright.switchToNextTab(); // $ExpectType void
+playwright.switchToPreviousTab(); // $ExpectType void
+playwright.closeCurrentTab(); // $ExpectType void
+playwright.closeOtherTabs(); // $ExpectType void
+playwright.openNewTab(); // $ExpectType void
 playwright.grabNumberOfOpenTabs(); // $ExpectType Promise<number>
-playwright.seeElement(str_pl); // $ExpectType Promise<any>
-playwright.dontSeeElement(str_pl); // $ExpectType Promise<any>
-playwright.seeElementInDOM(str_pl); // $ExpectType Promise<any>
-playwright.dontSeeElementInDOM(str_pl); // $ExpectType Promise<any>
+playwright.seeElement(str_pl); // $ExpectType void
+playwright.dontSeeElement(str_pl); // $ExpectType void
+playwright.seeElementInDOM(str_pl); // $ExpectType void
+playwright.dontSeeElementInDOM(str_pl); // $ExpectType void
 playwright.handleDownloads(); // $ExpectType Promise<void>
-playwright.click(str_pl); // $ExpectType Promise<any>
-playwright.click(str_pl, str_pl); // $ExpectType Promise<any>
-playwright.click(str_pl, null, { position }); // $ExpectType Promise<any>
-playwright.clickLink(); // $ExpectType Promise<any>
-playwright.forceClick(str_pl); // $ExpectType Promise<any>
-playwright.doubleClick(str_pl); // $ExpectType Promise<any>
-playwright.rightClick(str_pl); // $ExpectType Promise<any>
-playwright.checkOption(str_pl); // $ExpectType Promise<any>
-playwright.uncheckOption(str_pl); // $ExpectType Promise<any>
-playwright.seeCheckboxIsChecked(str_pl); // $ExpectType Promise<any>
-playwright.dontSeeCheckboxIsChecked(str_pl); // $ExpectType Promise<any>
-playwright.pressKeyDown(str_pl); // $ExpectType Promise<any>
-playwright.pressKeyUp(str_pl); // $ExpectType Promise<any>
-playwright.pressKey(str_pl); // $ExpectType Promise<any>
-playwright.type(str_pl); // $ExpectType Promise<any>
-playwright.fillField(str_pl, str_pl); // $ExpectType Promise<any>
-playwright.clearField(str_pl); // $ExpectType Promise<any>
-playwright.appendField(str_pl, str_pl); // $ExpectType Promise<any>
-playwright.seeInField(str_pl, str_pl); // $ExpectType Promise<any>
-playwright.dontSeeInField(str_pl, str_pl); // $ExpectType Promise<any>
-playwright.attachFile(str_pl, str_pl); // $ExpectType Promise<any>
-playwright.selectOption(str_pl, str_pl); // $ExpectType Promise<any>
+playwright.click(str_pl); // $ExpectType void
+playwright.click(str_pl, str_pl); // $ExpectType void
+playwright.click(str_pl, null, { position }); // $ExpectType void
+playwright.clickLink(); // $ExpectType void
+playwright.forceClick(str_pl); // $ExpectType void
+playwright.doubleClick(str_pl); // $ExpectType void
+playwright.rightClick(str_pl); // $ExpectType void
+playwright.checkOption(str_pl); // $ExpectType void
+playwright.uncheckOption(str_pl); // $ExpectType void
+playwright.seeCheckboxIsChecked(str_pl); // $ExpectType void
+playwright.dontSeeCheckboxIsChecked(str_pl); // $ExpectType void
+playwright.pressKeyDown(str_pl); // $ExpectType void
+playwright.pressKeyUp(str_pl); // $ExpectType void
+playwright.pressKey(str_pl); // $ExpectType void
+playwright.type(str_pl); // $ExpectType void
+playwright.fillField(str_pl, str_pl); // $ExpectType void
+playwright.clearField(str_pl); // $ExpectType void
+playwright.appendField(str_pl, str_pl); // $ExpectType void
+playwright.seeInField(str_pl, str_pl); // $ExpectType void
+playwright.dontSeeInField(str_pl, str_pl); // $ExpectType void
+playwright.attachFile(str_pl, str_pl); // $ExpectType void
+playwright.selectOption(str_pl, str_pl); // $ExpectType void
 playwright.grabNumberOfVisibleElements(str_pl); // $ExpectType Promise<number>
-playwright.seeInCurrentUrl(str_pl); // $ExpectType Promise<any>
-playwright.dontSeeInCurrentUrl(str_pl); // $ExpectType Promise<any>
-playwright.seeCurrentUrlEquals(str_pl); // $ExpectType Promise<any>
-playwright.dontSeeCurrentUrlEquals(str_pl); // $ExpectType Promise<any>
-playwright.see(str_pl); // $ExpectType Promise<any>
-playwright.seeTextEquals(str_pl); // $ExpectType Promise<any>
-playwright.dontSee(str_pl); // $ExpectType Promise<any>
+playwright.seeInCurrentUrl(str_pl); // $ExpectType void
+playwright.dontSeeInCurrentUrl(str_pl); // $ExpectType void
+playwright.seeCurrentUrlEquals(str_pl); // $ExpectType void
+playwright.dontSeeCurrentUrlEquals(str_pl); // $ExpectType void
+playwright.see(str_pl); // $ExpectType void
+playwright.seeTextEquals(str_pl); // $ExpectType void
+playwright.dontSee(str_pl); // $ExpectType void
 playwright.grabSource(); // $ExpectType Promise<string>
 playwright.grabBrowserLogs(); // $ExpectType Promise<any[]>
 playwright.grabCurrentUrl(); // $ExpectType Promise<string>
-playwright.seeInSource(str_pl); // $ExpectType Promise<any>
-playwright.dontSeeInSource(str_pl); // $ExpectType Promise<any>
-playwright.seeNumberOfElements(str_pl, num_pl); // $ExpectType Promise<any>
-playwright.seeNumberOfVisibleElements(str_pl, num_pl); // $ExpectType Promise<any>
-playwright.setCookie({ name: str_pl, value: str_pl}); // $ExpectType Promise<any>
-playwright.seeCookie(str_pl); // $ExpectType Promise<any>
-playwright.dontSeeCookie(str_pl); // $ExpectType Promise<any>
+playwright.seeInSource(str_pl); // $ExpectType void
+playwright.dontSeeInSource(str_pl); // $ExpectType void
+playwright.seeNumberOfElements(str_pl, num_pl); // $ExpectType void
+playwright.seeNumberOfVisibleElements(str_pl, num_pl); // $ExpectType void
+playwright.setCookie({ name: str_pl, value: str_pl}); // $ExpectType void
+playwright.seeCookie(str_pl); // $ExpectType void
+playwright.dontSeeCookie(str_pl); // $ExpectType void
 playwright.grabCookie(); // $ExpectType Promise<string[]> | Promise<string>
-playwright.clearCookie(); // $ExpectType Promise<any>
-playwright.executeScript(() => {}); // $ExpectType Promise<any>
+playwright.clearCookie(); // $ExpectType void
+playwright.executeScript(() => {}); // $ExpectType void
 playwright.grabTextFrom(str_pl); // $ExpectType Promise<string>
 playwright.grabTextFromAll(str_pl); // $ExpectType Promise<string[]>
 playwright.grabValueFrom(str_pl); // $ExpectType Promise<string>
@@ -98,33 +98,33 @@ playwright.grabHTMLFrom(str_pl); // $ExpectType Promise<string>
 playwright.grabHTMLFromAll(str_pl); // $ExpectType Promise<string[]>
 playwright.grabCssPropertyFrom(str_pl, str_pl); // $ExpectType Promise<string>
 playwright.grabCssPropertyFromAll(str_pl, str_pl); // $ExpectType Promise<string[]>
-playwright.seeCssPropertiesOnElements(str_pl, str_pl); // $ExpectType Promise<any>
-playwright.seeAttributesOnElements(str_pl, str_pl); // $ExpectType Promise<any>
-playwright.dragSlider(str_pl, num_pl); // $ExpectType Promise<any>
+playwright.seeCssPropertiesOnElements(str_pl, str_pl); // $ExpectType void
+playwright.seeAttributesOnElements(str_pl, str_pl); // $ExpectType void
+playwright.dragSlider(str_pl, num_pl); // $ExpectType void
 playwright.grabAttributeFrom(str_pl, str_pl); // $ExpectType Promise<string>
 playwright.grabAttributeFromAll(str_pl, str_pl); // $ExpectType Promise<string[]>
-playwright.saveElementScreenshot(str_pl, str_pl); // $ExpectType Promise<any>
-playwright.saveScreenshot(str_pl); // $ExpectType Promise<any>
+playwright.saveElementScreenshot(str_pl, str_pl); // $ExpectType void
+playwright.saveScreenshot(str_pl); // $ExpectType void
 playwright.makeApiRequest(str_pl, str_pl, str_pl); // $ExpectType Promise<object>
-playwright.wait(num_pl); // $ExpectType Promise<any>
-playwright.waitForEnabled(str_pl); // $ExpectType Promise<any>
-playwright.waitForValue(str_pl, str_pl); // $ExpectType Promise<any>
-playwright.waitNumberOfVisibleElements(str_pl, num_pl); // $ExpectType Promise<any>
-playwright.waitForClickable(str_pl); // $ExpectType Promise<any>
-playwright.waitForElement(str_pl); // $ExpectType Promise<any>
-playwright.waitForVisible(str_pl); // $ExpectType Promise<any>
-playwright.waitForInvisible(str_pl); // $ExpectType Promise<any>
-playwright.waitToHide(str_pl); // $ExpectType Promise<any>
-playwright.waitInUrl(str_pl); // $ExpectType Promise<any>
-playwright.waitUrlEquals(str_pl); // $ExpectType Promise<any>
-playwright.waitForText(str_pl); // $ExpectType Promise<any>
-playwright.waitForRequest(str_pl); // $ExpectType Promise<any>
-playwright.waitForResponse(str_pl); // $ExpectType Promise<any>
-playwright.switchTo(); // $ExpectType Promise<any>
-playwright.waitForFunction(() => { }); // $ExpectType Promise<any>
-playwright.waitForNavigation(str_pl); // $ExpectType Promise<any>
-playwright.waitForDetached(str_pl); // $ExpectType Promise<any>
+playwright.wait(num_pl); // $ExpectType void
+playwright.waitForEnabled(str_pl); // $ExpectType void
+playwright.waitForValue(str_pl, str_pl); // $ExpectType void
+playwright.waitNumberOfVisibleElements(str_pl, num_pl); // $ExpectType void
+playwright.waitForClickable(str_pl); // $ExpectType void
+playwright.waitForElement(str_pl); // $ExpectType void
+playwright.waitForVisible(str_pl); // $ExpectType void
+playwright.waitForInvisible(str_pl); // $ExpectType void
+playwright.waitToHide(str_pl); // $ExpectType void
+playwright.waitInUrl(str_pl); // $ExpectType void
+playwright.waitUrlEquals(str_pl); // $ExpectType void
+playwright.waitForText(str_pl); // $ExpectType void
+playwright.waitForRequest(str_pl); // $ExpectType void
+playwright.waitForResponse(str_pl); // $ExpectType void
+playwright.switchTo(); // $ExpectType void
+playwright.waitForFunction(() => { }); // $ExpectType void
+playwright.waitForNavigation(str_pl); // $ExpectType void
+playwright.waitForDetached(str_pl); // $ExpectType void
 playwright.grabDataFromPerformanceTiming(); // $ExpectType Promise<any>
 playwright.grabElementBoundingRect(str_pl); // $ExpectType Promise<number> | Promise<DOMRect>
-playwright.mockRoute(str_pl); // $ExpectType Promise<any>
-playwright.stopMockingRoute(str_pl); // $ExpectType Promise<any>
+playwright.mockRoute(str_pl); // $ExpectType void
+playwright.stopMockingRoute(str_pl); // $ExpectType void
