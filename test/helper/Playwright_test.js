bcd59a570573f35e370cad6460884b7a34b7877b
kobenguyent <kobenguyent@gmail.com>
2023-06-08 08:15:33 +0200

    fix(pw): drag n drop issue

--- test/helper/Playwright_test.js
+++ test/helper/Playwright_test.js
@@ -705,7 +705,7 @@ describe('Playwright', function () {
       }));
   });
 
-  describe('#dragAndDrop', () => {
+  describe.only('#dragAndDrop', () => {
     it('Drag item from source to target (no iframe) @dragNdrop', () => I.amOnPage('https://jqueryui.com/resources/demos/droppable/default.html')
       .then(() => I.seeElementInDOM('#draggable'))
       .then(() => I.dragAndDrop('#draggable', '#droppable'))
