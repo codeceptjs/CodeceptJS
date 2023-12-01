03fd3549bd86b19802f6e74077676c6836c74b2b
kobenguyent <kobenguyent@gmail.com>
2023-12-01 14:12:16 +0100

    try out webdriver safari

---
+++ test/acceptance/codecept.WebDriver.mac.js
@@ -0,0 +1,41 @@
+const TestHelper = require('../support/TestHelper');
+
+module.exports.config = {
+  tests: './*_test.js',
+  timeout: 10000,
+  output: './output',
+  helpers: {
+    WebDriver: {
+      url: TestHelper.siteUrl(),
+      browser: 'safari',
+      host: TestHelper.seleniumHost(),
+      port: TestHelper.seleniumPort(),
+      // disableScreenshots: true,
+      // desiredCapabilities: {
+      //   chromeOptions: {
+      //     args: ['--headless', '--disable-gpu', '--window-size=1280,1024'],
+      //   },
+      // },
+    },
+    ScreenshotSessionHelper: {
+      require: '../support/ScreenshotSessionHelper.js',
+      outputPath: './output',
+    },
+    Expect: {},
+  },
+  include: {},
+  bootstrap: async () => new Promise(done => {
+    setTimeout(done, 5000);
+  }), // let's wait for selenium
+  mocha: {},
+  name: 'acceptance',
+  plugins: {
+    screenshotOnFail: {
+      enabled: true,
+    },
+  },
+  gherkin: {
+    features: './gherkin/*.feature',
+    steps: ['./gherkin/steps.js'],
+  },
+};
