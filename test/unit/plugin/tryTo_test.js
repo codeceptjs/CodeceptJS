d55aaed49b3329cb0398675350955384873ce044
kobenguyent <kobenguyent@gmail.com>
2024-03-07 11:30:57 +0100

    fix: tryTo plugin

--- test/unit/plugin/tryTo_test.js
+++ test/unit/plugin/tryTo_test.js
@@ -8,17 +8,17 @@ describe('tryTo plugin', () => {
     recorder.start();
   });
 
-  it('should execute command on success', async () => {
-    const ok = await tryTo(() => recorder.add('test', () => 5));
-    expect(ok).to.false;
-    return recorder.promise();
-  });
-
   it('should execute command on fail', async () => {
     const notOk = await tryTo(() => recorder.add('test', () => {
       throw new Error('Ups');
     }));
     expect(false).is.equal(notOk);
+    return recorder.promise();
+  });
+
+  it('should execute command on success', async () => {
+    const ok = await tryTo(() => recorder.add('test', () => 5));
+    expect(ok).to.true;
     return recorder.promise();
   });
 });
