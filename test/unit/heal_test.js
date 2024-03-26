b4d18be3e73f92bb87eb5ff0f06d4e947cafff04
davert <davert.ua@gmail.com>
2024-03-26 03:28:52 +0200

    fixed loading chai in tests

--- test/unit/heal_test.js
+++ test/unit/heal_test.js
@@ -1,4 +1,7 @@
-const { expect } = require('chai');
+let expect;
+import('chai').then(chai => {
+  expect = chai.expect;
+});
 const heal = require('../../lib/heal');
 const recorder = require('../../lib/recorder');
 const Step = require('../../lib/step');
