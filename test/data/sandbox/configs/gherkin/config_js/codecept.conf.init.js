c886a09ac3809e93f64f34d91396a4c1968b5a50
kobenguyent <kobenguyent@gmail.com>
2024-06-06 10:39:38 +0200

    fix: eslint doesnt work

--- test/data/sandbox/configs/gherkin/config_js/codecept.conf.init.js
+++ test/data/sandbox/configs/gherkin/config_js/codecept.conf.init.js
@@ -1,16 +1,16 @@
 /** @type {CodeceptJS.MainConfig} */
 exports.config = {
-  tests: "./*_test.js",
-  output: "./output",
+  tests: './*_test.js',
+  output: './output',
   helpers: {
     Playwright: {
-      browser: "chromium",
-      url: "http://localhost",
+      browser: 'chromium',
+      url: 'http://localhost',
       show: true,
     },
   },
   include: {
-    I: "./steps_file.js",
+    I: './steps_file.js',
   },
-  name: "CodeceptJS",
+  name: 'CodeceptJS',
 };
