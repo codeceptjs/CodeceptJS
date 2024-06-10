d3dced1293703a049dd6443ca7076a92d8d8b3f1
KobeN <7845001+kobenguyent@users.noreply.github.com>
2024-06-10 06:31:17 +0200

    fix(cli): stale process (#4367)

--- test/acceptance/retryTo_test.js
+++ test/acceptance/retryTo_test.js
@@ -14,3 +14,23 @@ Scenario('retryTo works with non await steps @plugin', async () => {
     if (tryNum < 3) I.waitForVisible('.nothing', 1);
   }, 4);
 });
+
+Scenario('Should be succeed', async ({ I }) => {
+  I.amOnPage('http://example.org');
+  I.waitForVisible('.nothing', 1); // should fail here but it won't terminate
+  await retryTo((tryNum) => {
+    I.see('.doesNotMatter');
+  }, 10);
+});
+
+Scenario('Should fail after reached max retries', async () => {
+  await retryTo(() => {
+    throw new Error('Custom pluginRetryTo Error');
+  }, 3);
+});
+
+Scenario('Should succeed at the third attempt @plugin', async () => {
+  await retryTo(async (tryNum) => {
+    if (tryNum < 2) throw new Error('Custom pluginRetryTo Error');
+  }, 3);
+});
