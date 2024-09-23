6f5f03b79abbf45eae39965c6b6dc09dfe0d2ffe
kobenguyent <kobenguyent@gmail.com>
2024-09-23 16:38:33 +0200

    fix: gherkin_test and scenario stale test

--- test/runner/gherkin_test.js
+++ test/runner/gherkin_test.js
@@ -1,52 +1,52 @@
-const assert = require('assert')
-const path = require('path')
-const fs = require('fs')
-const exec = require('child_process').exec
+import assert from 'assert';
+import path from 'path';
+import fs from 'fs';
+import { exec } from 'child_process';
 
-const runner = path.join(__dirname, '/../../bin/codecept.js')
-const codecept_dir = path.join(__dirname, '/../data/sandbox/configs/gherkin/')
+const runner = path.join(path.dirname(import.meta.url), '/../../bin/codecept.js');
+const codecept_dir = path.join(path.dirname(import.meta.url), '/../data/sandbox/configs/gherkin/');
 
 describe('gherkin bdd commands', () => {
   describe('bdd:init', () => {
-    const codecept_dir_js = path.join(codecept_dir, 'config_js')
-    const codecept_dir_ts = path.join(codecept_dir, 'config_ts')
+    const codecept_dir_js = path.join(codecept_dir, 'config_js');
+    const codecept_dir_ts = path.join(codecept_dir, 'config_ts');
 
     beforeEach(() => {
       fs.copyFileSync(
         path.join(codecept_dir_js, 'codecept.conf.init.js'),
         path.join(codecept_dir_js, 'codecept.conf.js'),
-      )
+      );
       fs.copyFileSync(
         path.join(codecept_dir_ts, 'codecept.conf.init.ts'),
         path.join(codecept_dir_ts, 'codecept.conf.ts'),
-      )
-    })
+      );
+    });
 
     afterEach(() => {
       try {
-        fs.rmSync(path.join(codecept_dir_js, 'codecept.conf.js'))
+        fs.rmSync(path.join(codecept_dir_js, 'codecept.conf.js'));
         fs.rmSync(path.join(codecept_dir_js, 'features'), {
           recursive: true,
-        })
+        });
         fs.rmSync(path.join(codecept_dir_js, 'step_definitions'), {
           recursive: true,
-        })
+        });
       } catch (e) {
         // catch some error
       }
       try {
-        fs.rmSync(path.join(codecept_dir_ts, 'codecept.conf.ts'))
+        fs.rmSync(path.join(codecept_dir_ts, 'codecept.conf.ts'));
         fs.rmSync(path.join(codecept_dir_ts, 'features'), {
           recursive: true,
-        })
+        });
         fs.rmSync(path.join(codecept_dir_ts, 'step_definitions'), {
           recursive: true,
-        })
+        });
       } catch (e) {
         // catch some error
       }
-    })
-    ;[
+    });
+    [
       {
         codecept_dir_test: codecept_dir_js,
         extension: 'js',
@@ -58,23 +58,23 @@ describe('gherkin bdd commands', () => {
     ].forEach(({ codecept_dir_test, extension }) => {
       it(`prepare CodeceptJS to run feature files (codecept.conf.${extension})`, (done) => {
         exec(`${runner} gherkin:init ${codecept_dir_test}`, (err, stdout) => {
-          let dir = path.join(codecept_dir_test, 'features')
+          let dir = path.join(codecept_dir_test, 'features');
 
-          stdout.should.include('Initializing Gherkin (Cucumber BDD) for CodeceptJS')
-          stdout.should.include(`Created ${dir}, place your *.feature files in it`)
-          stdout.should.include('Created sample feature file: features/basic.feature')
+          stdout.should.include('Initializing Gherkin (Cucumber BDD) for CodeceptJS');
+          stdout.should.include(`Created ${dir}, place your *.feature files in it`);
+          stdout.should.include('Created sample feature file: features/basic.feature');
 
-          dir = path.join(codecept_dir_test, 'step_definitions')
-          stdout.should.include(`Created ${dir}, place step definitions into it`)
-          stdout.should.include(`Created sample steps file: step_definitions/steps.${extension}`)
-          assert(!err)
+          dir = path.join(codecept_dir_test, 'step_definitions');
+          stdout.should.include(`Created ${dir}, place step definitions into it`);
+          stdout.should.include(`Created sample steps file: step_definitions/steps.${extension}`);
+          assert(!err);
 
-          const configResult = fs.readFileSync(path.join(codecept_dir_test, `codecept.conf.${extension}`)).toString()
-          configResult.should.contain("features: './features/*.feature'")
-          configResult.should.contain(`steps: ['./step_definitions/steps.${extension}']`)
-          done()
-        })
-      })
-    })
-  })
-})
+          const configResult = fs.readFileSync(path.join(codecept_dir_test, `codecept.conf.${extension}`)).toString();
+          configResult.should.contain("features: './features/*.feature'");
+          configResult.should.contain(`steps: ['./step_definitions/steps.${extension}']`);
+          done();
+        });
+      });
+    });
+  });
+});
--- test/runner/scenario_stale_test.js
+++ test/runner/scenario_stale_test.js
@@ -1,22 +1,26 @@
-const { expect } = require('expect')
-const path = require('path')
-const { exec } = require('child_process')
+import { expect } from 'expect';
+import path from 'path';
+import { exec } from 'child_process';
+import { fileURLToPath } from 'url';
 
-const runner = path.join(__dirname, '/../../bin/codecept.js')
-const codecept_dir = path.join(__dirname, '/../data/sandbox')
-const codecept_run = `${runner} run`
-const config_run_config = (config) => `${codecept_run} --config ${codecept_dir}/${config}`
+// ESM doesn't have __dirname, so we calculate it from import.meta.url
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const runner = path.join(__dirname, '/../../bin/codecept.js');
+const codecept_dir = path.join(__dirname, '/../data/sandbox');
+const codecept_run = `${runner} run`;
+const config_run_config = (config) => `${codecept_run} --config ${codecept_dir}/${config}`;
 
 describe('Scenario termination check', () => {
   before(() => {
-    process.chdir(codecept_dir)
-  })
+    process.chdir(codecept_dir);
+  });
 
   it('Should always fail and terminate', (done) => {
     exec(config_run_config('codecept.scenario-stale.js'), (err, stdout) => {
-      expect(stdout).toContain('should not stale scenario error') // feature
-      expect(err).toBeTruthy()
-      done()
-    })
-  })
-})
+      expect(stdout).toContain('should not stale scenario error'); // feature
+      expect(err).toBeTruthy();
+      done();
+    });
+  });
+});
