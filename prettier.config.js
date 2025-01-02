9181a910c69e141b69010053d35527e749c6d764
kobenguyent <kobenguyent@gmail.com>
2025-01-02 16:47:04 +0100

    fix: grabTextFrom fails with webdriver

--- prettier.config.js
+++ prettier.config.js
@@ -2,9 +2,9 @@ module.exports = {
   printWidth: 220,
   tabWidth: 2,
   useTabs: false,
-  semi: true,
+  semi: false,
   singleQuote: true,
   trailingComma: 'all',
   bracketSameLine: false,
   arrowParens: 'avoid',
-};
+}
