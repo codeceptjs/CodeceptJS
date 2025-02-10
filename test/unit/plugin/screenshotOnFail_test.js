4f5c1c0ef5b4bc0c26c6909447176b0033d5f2a2
DavertMik <davert@testomat.io>
2025-02-10 02:17:17 +0200

    Fixed / in failed screenshot

--- test/unit/plugin/screenshotOnFail_test.js
+++ test/unit/plugin/screenshotOnFail_test.js
@@ -8,7 +8,8 @@ const screenshotOnFail = require('../../../lib/plugin/screenshotOnFail')
 const container = require('../../../lib/container')
 const event = require('../../../lib/event')
 const recorder = require('../../../lib/recorder')
-
+const { createTest } = require('../../../lib/mocha/test')
+const { deserializeSuite } = require('../../../lib/mocha/suite')
 let screenshotSaved
 
 describe('screenshotOnFail', () => {
@@ -25,7 +26,7 @@ describe('screenshotOnFail', () => {
 
   it('should remove the . at the end of test title', async () => {
     screenshotOnFail({})
-    event.dispatcher.emit(event.test.failed, { title: 'test title.' })
+    event.dispatcher.emit(event.test.failed, createTest('test title.'))
     await recorder.promise()
     expect(screenshotSaved.called).is.ok
     expect('test_title.failed.png').is.equal(screenshotSaved.getCall(0).args[0])
@@ -33,7 +34,7 @@ describe('screenshotOnFail', () => {
 
   it('should exclude the data driven in failed screenshot file name', async () => {
     screenshotOnFail({})
-    event.dispatcher.emit(event.test.failed, { title: 'Scenario with data driven | {"login":"admin","password":"123456"}' })
+    event.dispatcher.emit(event.test.failed, createTest('Scenario with data driven | {"login":"admin","password":"123456"}'))
     await recorder.promise()
     expect(screenshotSaved.called).is.ok
     expect('Scenario_with_data_driven.failed.png').is.equal(screenshotSaved.getCall(0).args[0])
@@ -41,7 +42,7 @@ describe('screenshotOnFail', () => {
 
   it('should create screenshot on fail', async () => {
     screenshotOnFail({})
-    event.dispatcher.emit(event.test.failed, { title: 'test1' })
+    event.dispatcher.emit(event.test.failed, createTest('test1'))
     await recorder.promise()
     expect(screenshotSaved.called).is.ok
     expect('test1.failed.png').is.equal(screenshotSaved.getCall(0).args[0])
@@ -50,17 +51,34 @@ describe('screenshotOnFail', () => {
   it('should create screenshot with unique name', async () => {
     screenshotOnFail({ uniqueScreenshotNames: true })
 
-    const test = { title: 'test1', uid: 1 }
+    const test = createTest('test1')
+    const suite = deserializeSuite({ title: 'suite1' })
+    test.addToSuite(suite)
+
     event.dispatcher.emit(event.test.failed, test)
     await recorder.promise()
     expect(screenshotSaved.called).is.ok
+    expect(screenshotSaved.getCall(0).args[0]).not.to.include('/')
     expect(`test1_${test.uid}.failed.png`).is.equal(screenshotSaved.getCall(0).args[0])
   })
 
   it('should create screenshot with unique name when uid is null', async () => {
     screenshotOnFail({ uniqueScreenshotNames: true })
 
-    event.dispatcher.emit(event.test.failed, { title: 'test1' })
+    const test = createTest('test1')
+    event.dispatcher.emit(event.test.failed, test)
+    await recorder.promise()
+    expect(screenshotSaved.called).is.ok
+    const fileName = screenshotSaved.getCall(0).args[0]
+    const regexpFileName = /test1_[0-9]{10}.failed.png/
+    expect(fileName.match(regexpFileName).length).is.equal(1)
+  })
+
+  it('should create screenshot with unique name when uid is null', async () => {
+    screenshotOnFail({ uniqueScreenshotNames: true })
+
+    const test = createTest('test1')
+    event.dispatcher.emit(event.test.failed, test)
     await recorder.promise()
     expect(screenshotSaved.called).is.ok
     const fileName = screenshotSaved.getCall(0).args[0]
@@ -70,14 +88,16 @@ describe('screenshotOnFail', () => {
 
   it('should not save screenshot in BeforeSuite', async () => {
     screenshotOnFail({ uniqueScreenshotNames: true })
-    event.dispatcher.emit(event.test.failed, { title: 'test1' }, null, 'BeforeSuite')
+    const test = createTest('test1')
+    event.dispatcher.emit(event.test.failed, test, null, 'BeforeSuite')
     await recorder.promise()
     expect(!screenshotSaved.called).is.ok
   })
 
   it('should not save screenshot in AfterSuite', async () => {
     screenshotOnFail({ uniqueScreenshotNames: true })
-    event.dispatcher.emit(event.test.failed, { title: 'test1' }, null, 'AfterSuite')
+    const test = createTest('test1')
+    event.dispatcher.emit(event.test.failed, test, null, 'AfterSuite')
     await recorder.promise()
     expect(!screenshotSaved.called).is.ok
   })
