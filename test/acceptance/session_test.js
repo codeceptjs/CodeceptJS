a0c5827fdeaa4a26c61cb25f730aa211e611fcfb
Thomas Hohn <tho@gyldendal.dk>
2025-03-13 10:43:28 +0100

    Tryout

--- test/acceptance/session_test.js
+++ test/acceptance/session_test.js
@@ -54,9 +54,6 @@ Scenario('Different cookies for different sessions @Playwright @Puppeteer', asyn
   session('john', () => {
     I.amOnPage(cookiePage)
   })
-  session('mary', () => {
-    I.amOnPage(cookiePage)
-  })
 
   cookies.default = (await I.grabCookie(cookieName)).value
   I.say(`${cookieName}: ${cookies.default}`)
@@ -64,6 +61,12 @@ Scenario('Different cookies for different sessions @Playwright @Puppeteer', asyn
     cookies.john = (await I.grabCookie(cookieName)).value
     I.say(`${cookieName}: ${cookies.john}`)
   })
+
+  session('mary', () => {
+    I.amOnPage(cookiePage)
+  })
+  cookies.default = (await I.grabCookie(cookieName)).value
+  I.say(`${cookieName}: ${cookies.default}`)
   session('mary', async () => {
     cookies.mary = (await I.grabCookie(cookieName)).value
     I.say(`${cookieName}: ${cookies.mary}`)
