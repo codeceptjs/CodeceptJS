f078caa6bd0dd185db3898e44fbc22d3bc4715aa
kobenguyent <7845001+kobenguyent@users.noreply.github.com>
2025-08-11 08:29:38 +0000

    fix: runner tests

--- test/data/sandbox/support/bdd_helper.js
+++ test/data/sandbox/support/bdd_helper.js
@@ -1,46 +1,50 @@
-import assert from 'assert';
-import helperModule from '../../../../lib/helper.js';
-const Helper = helperModule.default || helperModule;
+import assert from 'assert'
+import helperModule from '../../../../lib/helper.js'
+const Helper = helperModule.default || helperModule
 
 class CheckoutHelper extends Helper {
   _before() {
-    this.num = 0;
-    this.sum = 0;
-    this.discountCalc = null;
+    this.num = 0
+    this.sum = 0
+    this.discountCalc = null
   }
 
   addItem(price) {
-    this.num++;
-    this.sum += price;
+    if (typeof this.num !== 'number') this.num = 0
+    if (typeof this.sum !== 'number') this.sum = 0
+    this.num++
+    this.sum += price
   }
 
   seeNum(num) {
-    assert.equal(num, this.num);
+    if (typeof this.num !== 'number') this.num = 0
+    assert.equal(num, this.num)
   }
 
   seeSum(sum) {
-    assert.equal(sum, this.sum);
+    if (typeof this.sum !== 'number') this.sum = 0
+    assert.equal(sum, this.sum)
   }
 
   haveDiscountForPrice(price, discount) {
     this.discountCalc = () => {
       if (this.sum > price) {
-        this.sum -= this.sum * discount / 100;
+        this.sum -= (this.sum * discount) / 100
       }
-    };
+    }
   }
 
   addProduct(name, price) {
-    this.sum += price;
+    this.sum += price
   }
 
   checkout() {
     if (this.discountCalc) {
-      this.discountCalc();
+      this.discountCalc()
     }
   }
 
   login() {}
 }
 
-export default CheckoutHelper;
+export default CheckoutHelper
