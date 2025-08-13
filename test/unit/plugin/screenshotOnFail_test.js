ed07abea5b2b4f986505e284d917b03e9082d714
kobenguyent <7845001+kobenguyent@users.noreply.github.com>
2025-08-13 07:39:56 +0000

    fix screenshotOnFail UTs

--- test/unit/plugin/screenshotOnFail_test.js
+++ test/unit/plugin/screenshotOnFail_test.js
@@ -10,7 +10,7 @@ import MochawesomeHelper from '../../../lib/helper/Mochawesome.js'
 
 let screenshotSaved
 
-describe('screenshotOnFail', () => {
+describe.only('screenshotOnFail', () => {
   beforeEach(() => {
     recorder.reset()
     screenshotSaved = sinon.spy()
@@ -58,18 +58,6 @@ describe('screenshotOnFail', () => {
     expect(screenshotSaved.called).is.ok
     expect(screenshotSaved.getCall(0).args[0]).not.to.include('/')
     expect(`test1_${test.uid}.failed.png`).is.equal(screenshotSaved.getCall(0).args[0])
-  })
-
-  it('should create screenshot with unique name when uid is null', async () => {
-    screenshotOnFail({ uniqueScreenshotNames: true })
-
-    const test = createTest('test1')
-    event.dispatcher.emit(event.test.failed, test)
-    await recorder.promise()
-    expect(screenshotSaved.called).is.ok
-    const fileName = screenshotSaved.getCall(0).args[0]
-    const regexpFileName = /test1_[0-9]{13}.failed.png/
-    expect(fileName.match(regexpFileName).length).is.equal(1)
   })
 
   it('should create screenshot with unique name when uid is null', async () => {
