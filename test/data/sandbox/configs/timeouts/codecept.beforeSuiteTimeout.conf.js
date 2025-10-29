aaf4fef657e9fb413519d16b72ded0002490a542
Jaromir Obr <jaromir.obr@gmail.com>
2025-10-29 13:47:19 +0100

    fix: global timeout before suite (#5275)
    * Fix: preserve global timeout with BeforeSuite hook
    
    * test: clarify feature name for global timeout with BeforeSuite
    
    * chore: remove unused plugins field from beforeSuiteTimeout config

---
+++ test/data/sandbox/configs/timeouts/codecept.beforeSuiteTimeout.conf.js
@@ -0,0 +1,11 @@
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    CustomHelper: {
+      require: './customHelper.js',
+    },
+  },
+  name: 'beforeSuiteTimeout',
+  timeout: 2,
+}
