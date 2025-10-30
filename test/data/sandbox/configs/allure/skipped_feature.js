f55a991bf43b6cc4add389052ff0f6be8283bf24
kobenguyent <7845001+kobenguyent@users.noreply.github.com>
2025-10-30 13:59:34 +0100

    clean up files (#5281)

--- test/data/sandbox/configs/allure/skipped_feature.js
+++
@@ -1,9 +0,0 @@
-Feature.skip('Skipped test suite');
-
-Scenario('Test not run', async I => {
-  I.say('Test was fine.');
-});
-
-Scenario('Test not run', async I => {
-  I.say('Test was fine.');
-});
