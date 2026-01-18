d10f65307644ecafe85b68a94fa2e4d1c7ff6c02
Denys Kuchma <148628428+DenysKuchma@users.noreply.github.com>
2026-01-18 11:16:14 +0200

    correct TypeScript type definitions for effect functions (#5395)

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -522,9 +522,9 @@ declare namespace CodeceptJS {
   function addStep(step: string | RegExp, fn: Function): Promise<void>
 }
 
-type TryTo = <T>(fn: () => Promise<T> | T) => Promise<T | false>
-type HopeThat = <T>(fn: () => Promise<T> | T) => Promise<T | false>
-type RetryTo = <T>(fn: () => Promise<T> | T, retries?: number) => Promise<T>
+type TryTo = (fn: () => void) => Promise<boolean>
+type HopeThat = (fn: () => void) => Promise<boolean>
+type RetryTo = (fn: (tries: number) => Promise<void> | void, maxTries: number, pollInterval?: number) => Promise<boolean>
 
 // Globals
 declare const codecept_dir: string
