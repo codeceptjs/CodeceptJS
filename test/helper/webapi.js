f7bc6f55146cc41f74851fb4af31d89381d9f6db
Denys Kuchma <den.kuchma@ukr.net>
2026-03-05 15:03:12 +0200

    fix workers again

--- test/helper/webapi.js
+++ test/helper/webapi.js
@@ -1580,8 +1580,7 @@ export function tests() {
           },
         )
       } catch (e) {
-        expect(e.message).to.include('expected all elements ({css: a[href="/codeceptjs/CodeceptJS"]}) to have attributes {"disable":true}')
-        expect(e.message).to.match(/"0" to equal "\d+"/)
+        expect(e.message).to.include('expected all elements ({css: a[href="/codeceptjs/CodeceptJS"]}) to have attributes {"disable":true} "0" to equal "3"')
       }
     })
 
