bc8211c737e1f021b7f109ecc9507816c25db919
kobenguyent <7845001+kobenguyent@users.noreply.github.com>
2026-06-13 11:18:00 +0200

    fix(init): missing file ext of step file when using typescript (#5615)
    * fix(init): missing file ext of step file when using typescript
    
    * add more tests
    
    * add proper verification

--- test/runner/init_test.js
+++ test/runner/init_test.js
@@ -4,6 +4,9 @@ import path from 'path'
 import fs from 'fs'
 import { mkdirp } from 'mkdirp'
 import { fileURLToPath } from 'url'
+import sinon from 'sinon'
+import inquirer from 'inquirer'
+import * as initModule from '../../lib/command/init.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -13,29 +16,24 @@ const codecept_dir = path.join(__dirname, '/../data/sandbox/configs/init')
 describe('Init Command', function () {
   this.timeout(20000)
 
+  let promptStub;
+
   beforeEach(() => {
     mkdirp.sync(codecept_dir)
     process.env._INIT_DRY_RUN_INSTALL = true
+    process.env.CODECEPT_TEST = 'true'
+    promptStub = sinon.stub(inquirer, 'prompt')
   })
 
   afterEach(() => {
     try {
-      fs.unlinkSync(`${codecept_dir}/codecept.conf.ts`)
-      fs.unlinkSync(`${codecept_dir}/steps_file.ts`)
-      fs.unlinkSync(`${codecept_dir}/tsconfig.json`)
+      fs.rmSync(codecept_dir, { recursive: true, force: true })
     } catch (e) {
       // continue regardless of error
     }
-
-    try {
-      fs.unlinkSync(`${codecept_dir}/codecept.conf.js`)
-      fs.unlinkSync(`${codecept_dir}/steps_file.js`)
-      fs.unlinkSync(`${codecept_dir}/jsconfig.json`)
-    } catch (e) {
-      // continue regardless of error
-    }
-
+    sinon.restore()
     delete process.env._INIT_DRY_RUN_INSTALL
+    delete process.env.CODECEPT_TEST
   })
 
   it('should have init command available and noTranslation defined', async () => {
@@ -62,5 +60,48 @@ describe('Init Command', function () {
       }
       throw error
     }
+  })
+
+  it('should initialize a JS project', async () => {
+    // When yes: true, it skips prompts
+    await initModule.default(codecept_dir, { yes: true })
+
+    fs.existsSync(path.join(codecept_dir, 'codecept.conf.js')).should.be.true
+    fs.existsSync(path.join(codecept_dir, 'steps_file.js')).should.be.true
+
+    fs.readFile(path.join(codecept_dir, 'codecept.conf.js'), 'utf8', (err, data) => {
+      if (err) {
+        throw Error(err);
+        return;
+      }
+      data.should.contain('./steps_file.js');
+    });
+  })
+
+  it('should initialize a TS project', async () => {
+    promptStub.onCall(0).resolves({
+        typescript: true,
+        tests: './tests/*_test.ts',
+        helper: 'Playwright',
+        output: './output',
+    })
+    promptStub.onCall(1).resolves({
+        Playwright_browser: 'chromium',
+        Playwright_url: 'http://localhost',
+        Playwright_show: false,
+    })
+
+    await initModule.default(codecept_dir, { yes: false })
+
+    fs.existsSync(path.join(codecept_dir, 'codecept.conf.ts')).should.be.true
+    fs.existsSync(path.join(codecept_dir, 'steps_file.ts')).should.be.true
+
+    fs.readFile(path.join(codecept_dir, 'codecept.conf.ts'), 'utf8', (err, data) => {
+      if (err) {
+        throw Error(err);
+        return;
+      }
+      data.should.contain('./steps_file.ts');
+    });
   })
 })
